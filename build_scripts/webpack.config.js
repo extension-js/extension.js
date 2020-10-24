00c5b5789754bdc64ff24041782139093c11219d
Cezar Augusto <boss@cezaraugusto.net>
2020-10-24 19:30:53 -0300

    Add webextension-polyfill

--- build_scripts/webpack.config.js
+++ build_scripts/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const webpack = require('webpack')
 const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
@@ -10,16 +11,34 @@ module.exports = {
     port: 8081
   },
   entry: {
-    background: ['./src/background.script.js']
+    background: [
+      path.resolve(__dirname, '../src/background.script.js')
+    ]
   },
   output: {
-    path: path.resolve(__dirname, '..', 'package')
+    path: path.resolve(__dirname, '../package')
   },
   plugins: [
     new CopyPlugin({
       patterns: [
-        { from: 'public/manifest.json', to: 'manifest.json' }
+        { from: path.resolve(__dirname, '../public/manifest.json'), to: 'manifest.json' }
       ]
-    })
-  ]
+    }),
+    // This is needed only for non-compilant WebExtension APIs
+    // (all browsers but Firefox and Edge)
+    // TODO: Do not add this plugin when developing for those vendors
+    new webpack.ProvidePlugin({ browser: require.resolve('webextension-polyfill') })
+  ],
+  module: {
+    // Adapted from https://github.com/webextension-toolbox/webextension-toolbox,
+    // relased under MIT license. Copyright 2018 Henrik Wenz
+    rules: [{
+      test: /webextension-polyfill[\\/]+dist[\\/]+browser-polyfill\.js$/,
+      loader: require.resolve('string-replace-loader'),
+      options: {
+        search: 'typeof browser === "undefined"',
+        replace: 'typeof window.browser === "undefined" || Object.getPrototypeOf(window.browser) !== Object.prototype'
+      }
+    }]
+  }
 }
