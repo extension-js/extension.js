b7caad0091463a820e50ea6737486797f69d23bf
Cezar Augusto <boss@cezaraugusto.net>
2020-12-11 20:52:55 -0300

    Setup packages for publish time

--- create/createExtension.js
+++ create/createExtension.js
@@ -10,6 +10,7 @@ const importExternalTemplate = require('./steps/importExternalTemplate')
 const importLocalTemplate = require('./steps/importLocalTemplate')
 const writePackageJson = require('./steps/writePackageJson')
 const installDependencies = require('./steps/installDependencies')
+const symlinkLocalDependencies = require('./steps/symlinkLocalDependencies')
 const messages = require('./messages')
 const abortAndClean = require('./steps/abortProjectAndClean')
 const cleanTemplateFolder = require('./steps/cleanTemplateFolder')
@@ -26,6 +27,7 @@ module.exports = async function (workingDir, projectName, template) {
 
     await writePackageJson(workingDir, projectName, template)
     await installDependencies(workingDir, projectName)
+    await symlinkLocalDependencies(workingDir, projectName)
     await cleanTemplateFolder(template)
     await messages.successfulInstall(workingDir, projectName)
   } catch (error) {
