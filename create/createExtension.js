ddd18c2e06914bd8be235d1e4a2936e3672653cc
Cezar Augusto <boss@cezaraugusto.net>
2020-12-11 22:38:33 -0300

    Undo Lerna

--- create/createExtension.js
+++ create/createExtension.js
@@ -10,7 +10,6 @@ const importExternalTemplate = require('./steps/importExternalTemplate')
 const importLocalTemplate = require('./steps/importLocalTemplate')
 const writePackageJson = require('./steps/writePackageJson')
 const installDependencies = require('./steps/installDependencies')
-const symlinkLocalDependencies = require('./steps/symlinkLocalDependencies')
 const messages = require('./messages')
 const abortAndClean = require('./steps/abortProjectAndClean')
 const cleanTemplateFolder = require('./steps/cleanTemplateFolder')
@@ -27,7 +26,6 @@ module.exports = async function (workingDir, projectName, template) {
 
     await writePackageJson(workingDir, projectName, template)
     await installDependencies(workingDir, projectName)
-    await symlinkLocalDependencies(workingDir, projectName)
     await cleanTemplateFolder(template)
     await messages.successfulInstall(workingDir, projectName)
   } catch (error) {
