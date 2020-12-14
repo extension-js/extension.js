f2b16725129c962ed3608b85035fabedcfc1d31c
Cezar Augusto <boss@cezaraugusto.net>
2020-12-14 01:09:57 -0300

    Link extension-create instead of d/l (local)

--- create/steps/installDependencies.js
+++ create/steps/installDependencies.js
@@ -7,7 +7,7 @@
 
 const path = require('path')
 
-const pacote = require('pacote')
+const spawn = require('cross-spawn')
 const { log } = require('log-md')
 
 const abortProjectAndClean = require('./abortProjectAndClean')
@@ -17,29 +17,51 @@ module.exports = async function (workingDir, projectName) {
   const projectPackageJson = path.join(projectPath, 'package.json')
   const packageMetadata = require(projectPackageJson)
 
-  const dependencies = Object.entries({
-    ...packageMetadata.dependencies,
-    ...packageMetadata.devDependencies
-  })
+  const dependencies = packageMetadata.dependencies || []
+  const devDependencies = packageMetadata.devDependencies || []
 
-  if (dependencies.length === 0) {
+  if (
+    dependencies.length === 0 &&
+    devDependencies.length === 0
+  ) {
     log('⏭  - No dependencies. Skipping install step...')
 
     return
   }
 
-  log('🛠  - Installing dependencies...')
+  const command = 'npm'
+  const commonArgs = ['--prefix', projectPath]
 
   try {
-    for await (const dependency of dependencies) {
-      const [name, version] = dependency
+    log('🛠  - Installing extension-create as devDependency...')
+    // Link instead of download in local env
+    if (process.env.NODE_ENV === 'development') {
+      spawn(command, ['link'], { stdio: 'inherit' })
 
-      log(`   └── Installing \`${name}\`@_${version}_...`)
-      pacote.extract(
-        `${name}@${version}`,
-        `${projectPath}/node_modules`
-      )
+      const linkSelfArgs = [
+        commonArgs,
+        `${projectPath}/node_modules`,
+        'link',
+        'extension-create'
+      ]
+
+      spawn(command, linkSelfArgs, { stdio: 'inherit' })
+    } else {
+      const installSelfArgs = [
+        ...commonArgs,
+        'install',
+        '-D',
+        'extension-create'
+      ]
+
+      spawn(command, installSelfArgs, { stdio: 'inherit' })
     }
+
+    log('⚙️  - Installing package dependencies...')
+    const installArgs = [...commonArgs, 'install', '--exact']
+
+    spawn(command, installArgs, { stdio: 'inherit' })
+    await Promise.resolve()
   } catch (error) {
     abortProjectAndClean(error, workingDir, projectName)
   }
