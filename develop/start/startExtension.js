decefd7a19304aea4841dedf7f1e40ebe7904a9e
Cezar Augusto <boss@cezaraugusto.net>
2020-12-22 23:37:04 -0300

    Basic support to kickstart an external extension

--- develop/start/startExtension.js
+++ develop/start/startExtension.js
@@ -5,16 +5,26 @@
 // ███████║   ██║   ██║  ██║██║  ██║   ██║
 // ╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝
 
+const path = require('path')
+
 const { log } = require('log-md')
+const githubPartials = require('github-partials')
 
 const resoleManifest = require('./steps/resolveManifest')
 const startWebpack = require('./steps/startWebpack')
 
-module.exports = async function (projectDir, manifestPath) {
-  try {
-    const resolvedManifest = await resoleManifest(projectDir, manifestPath)
+module.exports = async function (projectDir, { manifest, remote }) {
+  let currentProjectDir = projectDir
 
-    startWebpack(projectDir, resolvedManifest)
+  try {
+    if (remote) {
+      githubPartials(remote)
+      currentProjectDir = path.join(process.cwd(), path.basename(remote))
+    }
+
+    const resolvedManifest = await resoleManifest(currentProjectDir, manifest)
+
+    startWebpack(currentProjectDir, resolvedManifest)
   } catch (error) {
     log(`
       Error while starting the extension: ${error}
