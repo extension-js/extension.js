e0839580bb9ed486c58797d5db652547f67b2712
Cezar Augusto <boss@cezaraugusto.net>
2020-12-24 20:01:48 -0300

    Allow optional background scripts

--- develop/start/resolve/resolveBackgroundScriptEntries.js
+++ develop/start/resolve/resolveBackgroundScriptEntries.js
@@ -9,8 +9,7 @@ const path = require('path')
 
 module.exports = function (manifestPath) {
   const manifest = require(manifestPath)
-  const background = [...manifest.background.scripts]
 
-  return background
+  return manifest.background.scripts
     .map(script => path.resolve(path.dirname(manifestPath), script))
 }
