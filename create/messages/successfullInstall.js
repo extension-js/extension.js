8ee8a3ae7c376b3bc91e778f490d24b2b953a8ba
Cezar Augusto <boss@cezaraugusto.net>
2021-01-28 09:50:24 -0300

    Improve successful install result

--- create/messages/successfullInstall.js
+++ create/messages/successfullInstall.js
@@ -13,9 +13,9 @@ module.exports = function (workingDir, projectName) {
   const projectPath = path.join(workingDir, projectName)
   const relativePath = path.relative(workingDir, projectPath)
 
-  log(`
-    # Success! Extension ${projectName} created at ${relativePath}.
+  log(`  🧩 Success! Extension ${projectName} created.`)
 
+  log(`
     Now \`cd ${relativePath}\` and *npm start* to open a new browser instance
     with your extension installed, loaded, and ready for development.
 
