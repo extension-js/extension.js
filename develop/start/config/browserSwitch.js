2902df4b7ce1dc615ca0c54cbdc1121ec3c8c50e
Cezar Augusto <boss@cezaraugusto.net>
2021-03-14 19:34:37 -0300

    Support running both Edge and Chrome

--- develop/start/config/browserSwitch.js
+++ develop/start/config/browserSwitch.js
@@ -9,6 +9,21 @@ module.exports = function (projectDir, browserVendor) {
     case 'edge':
       return new RunEdgeExtension({extensionPath: projectDir})
 
+    case 'all':
+      return {
+        apply: (compiler) => {
+          new RunChromeExtension({
+            extensionPath: projectDir,
+            port: 8081
+          }).apply(compiler)
+
+          new RunEdgeExtension({
+            extensionPath: projectDir,
+            port: 8082
+          }).apply(compiler)
+        }
+      }
+
     default:
       // TODO: Should be the user default browser, not Chrome
       return new RunChromeExtension({extensionPath: projectDir})
