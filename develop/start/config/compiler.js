2f9bbcf0c8ef18bb32984eb9207e24eb224e78a4
Cezar Augusto <boss@cezaraugusto.net>
2021-06-17 22:57:14 -0300

    Clean-up develop:start dependencies

--- develop/start/config/compiler.js
+++ develop/start/config/compiler.js
@@ -5,39 +5,27 @@
 // ███████║   ██║   ██║  ██║██║  ██║   ██║
 // ╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝
 
-const webpack = require('webpack')
+const BrowserExtensionPolyfill = require('webpack-browser-extension-polyfill')
 
 const browserSwitch = require('./browserSwitch')
 
 process.on('unhandledRejection', (error) => { throw error })
 
-module.exports = (projectDir, {browserVendor}) => {
+module.exports = (projectDir, {browserVendor, manifestPath}) => {
   const config = {
     mode: 'development',
     // https://github.com/webpack/webpack/issues/2145
     devtool: 'inline-cheap-module-source-map',
     plugins: [
-      // Polyfill `browser` namespace for unspported browsers (FF).
-      // TODO: Do not add this plugin when developing for those vendors
-      new webpack.ProvidePlugin({browser: require.resolve('webextension-polyfill')}),
+      // Supports Add-On polyfilling where appropriate
+      browserVendor !== 'firefox'
+        ? new BrowserExtensionPolyfill({manifestPath})
+        : null,
       // Browser lists loaded conditionally based on user choice
       browserSwitch(projectDir, browserVendor)
     ],
     resolve: {
       extensions: ['js', '.json']
-    },
-    module: {
-      // Adapted from
-      // https://github.com/webextension-toolbox/webextension-toolbox
-      // Relased under MIT license. Copyright 2018 Henrik Wenz
-      rules: [{
-        test: /webextension-polyfill[\\/]+dist[\\/]+browser-polyfill\.js$/,
-        loader: require.resolve('string-replace-loader'),
-        options: {
-          search: 'typeof browser === "undefined"',
-          replace: 'typeof window.browser === "undefined" || Object.getPrototypeOf(window.browser) !== Object.prototype'
-        }
-      }]
     }
   }
 
