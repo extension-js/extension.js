fb5901c7100418ffc46aab05cacf101879f05c43
Cezar Augusto <boss@cezaraugusto.net>
2022-02-11 19:26:33 -0300

    Update webpack-run-chrome-extension@0.6.0

--- develop/start/config/compiler.js
+++ develop/start/config/compiler.js
@@ -10,15 +10,13 @@ const browserSwitch = require('./browserSwitch')
 module.exports = (projectDir, {browserVendor}) => {
   const config = {
     mode: 'development',
+    entry: {},
     // https://github.com/webpack/webpack/issues/2145
     devtool: 'inline-cheap-module-source-map',
     plugins: [
       // Browser lists loaded conditionally based on user choice
       browserSwitch(projectDir, browserVendor)
-    ],
-    resolve: {
-      extensions: ['js', '.json']
-    }
+    ]
   }
 
   return {
