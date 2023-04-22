b62cf6a0f2cd79552a897bf37d4af76a4f916348
itsbrex <roach7@gmail.com>
2023-04-22 01:52:05 -0700

    Refactor createExtension.js to use destructuring and improve error handling

--- create/createExtension.js
+++ create/createExtension.js
@@ -5,29 +5,34 @@
 // ╚██████╗██║  ██║███████╗██║  ██║   ██║   ███████╗
 //  ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝
 
-const createDirectory = require('./steps/createDirectory');
-const importExternalTemplate = require('./steps/importExternalTemplate');
-const importLocalTemplate = require('./steps/importLocalTemplate');
-const writePackageJson = require('./steps/writePackageJson');
-const installDependencies = require('./steps/installDependencies');
-const messages = require('./messages');
-const abortAndClean = require('./steps/abortProjectAndClean');
-const cleanTemplateFolder = require('./steps/cleanTemplateFolder');
+const createDirectory = require('./steps/createDirectory')
+const importExternalTemplate = require('./steps/importExternalTemplate')
+const importLocalTemplate = require('./steps/importLocalTemplate')
+const writePackageJson = require('./steps/writePackageJson')
+const installDependencies = require('./steps/installDependencies')
+const messages = require('./messages')
+const abortAndClean = require('./steps/abortProjectAndClean')
+const cleanTemplateFolder = require('./steps/cleanTemplateFolder')
 
-module.exports = async function createExtension(workingDir, projectName, template) {
-	const isExternalTemplate = !!template;
+module.exports = async function createExtension({
+  workingDir,
+  projectName,
+  template,
+  isExternalTemplate
+}) {
+  try {
+    await createDirectory(workingDir, projectName)
 
-	try {
-		await createDirectory(workingDir, projectName);
+    isExternalTemplate
+      ? await importExternalTemplate(workingDir, projectName, template)
+      : await importLocalTemplate(workingDir, projectName)
 
-		isExternalTemplate ? await importExternalTemplate(workingDir, projectName, template) : await importLocalTemplate(workingDir, projectName);
-
-		await writePackageJson(workingDir, projectName, template);
-		await installDependencies(workingDir, projectName);
-		await cleanTemplateFolder(template, isExternalTemplate);
-		messages.successfullInstall(workingDir, projectName);
-	} catch (error) {
-		await abortAndClean(error, workingDir, projectName);
-		await cleanTemplateFolder(template, isExternalTemplate);
-	}
-};
+    await writePackageJson(workingDir, projectName, template)
+    await installDependencies(workingDir, projectName)
+    await cleanTemplateFolder(template, isExternalTemplate)
+    messages.successfullInstall(workingDir, projectName)
+  } catch (error) {
+    await abortAndClean(error, workingDir, projectName)
+    await cleanTemplateFolder(template, isExternalTemplate)
+  }
+}
