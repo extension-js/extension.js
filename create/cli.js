3a5862b684772456bd02941665a51e43c3b7213d
itsbrex <roach7@gmail.com>
2023-04-21 23:29:18 -0700

    🚧  REFACTOR: refactoring the template options and commander command

--- create/cli.js
+++ create/cli.js
@@ -17,54 +17,75 @@ async function createExtensionCLI(clientProgram = program) {
   const inquirer = (await import('inquirer')).default
 
   clientProgram
-    .version(packageJson.version)
-    .command('create', {isDefault: true})
-    .usage('create <project-directory> [options]')
-    .action(async (projectDir, cmd) => {
-      let projectName
-      let templateName
+		.version(packageJson.version)
+		.command('create', { isDefault: true })
+		.usage('create <project-directory> [options]')
+		.action(async (projectDir, options) => {
+			let projectName;
+			let templateName;
 
-      // Check if projectDir is a string before assigning it to projectName
-      if (typeof projectDir === 'string') {
-        projectName = projectDir
-      } else {
-        const {useCwd, projectNameInput} = await inquirer.prompt([
-          {
-            type: 'confirm',
-            name: 'useCwd',
-            message:
-              'No project directory provided. Do you want to use the current directory?',
-            default: false
-          },
-          {
-            type: 'input',
-            name: 'projectNameInput',
-            message: 'Enter a project directory:',
-            when: (answers) => !answers.useCwd
-          }
-        ])
+			// Check if projectDir is a string before assigning it to projectName
+			if (typeof projectDir === 'string') {
+				projectName = projectDir;
+			} else {
+				const { useCwd, projectNameInput } = await inquirer.prompt([
+					{
+						type: 'confirm',
+						name: 'useCwd',
+						message: 'No project directory provided. Do you want to use the current directory?',
+						default: false,
+					},
+					{
+						type: 'input',
+						name: 'projectNameInput',
+						message: 'Enter a project directory:',
+						when: (answers) => !answers.useCwd,
+					},
+				]);
 
-        if (useCwd) {
-          projectName = '.'
-        } else {
-          projectName = projectNameInput
-        }
-      }
+				if (useCwd) {
+					projectName = '.';
+				} else {
+					projectName = projectNameInput;
+				}
+			}
 
-      // eslint-disable-next-line prefer-const
-      templateName = cmd.template
+			// eslint-disable-next-line prefer-const
+			if (typeof options.template === 'undefined') {
+				const { template } = await inquirer.prompt([
+					{
+						type: 'list',
+						name: 'template',
+						message: 'Select a template:',
+						choices: [
+							{
+								name: 'Standard',
+								value: 'standard',
+							},
+							{
+								name: 'Popup',
+								value: 'popup',
+							},
+						],
+					},
+				]);
 
-      // Call the createExtension function here, within the action callback
-      const workingDir = process.cwd()
+				templateName = template;
+			} else {
+				templateName = options.template;
+			}
 
-      await createExtension(workingDir, projectName, templateName)
-    })
+			// Call the createExtension function here, within the action callback
+			const workingDir = process.cwd();
+
+			await createExtension(workingDir, projectName, templateName);
+		});
 
   // Parse the command-line arguments
   clientProgram.parse(process.argv)
 }
 
-// If the module was called from the cmd line, execute it
+// If the module was called from the options line, execute it
 if (require.main === module) {
   createExtensionCLI()
 }
