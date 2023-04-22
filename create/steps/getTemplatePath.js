ce068e96dec436e4437e5a28f120380047032f89
itsbrex <roach7@gmail.com>
2023-04-22 00:05:07 -0700

    Refactor getTemplatePath to accept an isExternalTemplate flag and handle it accordingly

--- create/steps/getTemplatePath.js
+++ create/steps/getTemplatePath.js
@@ -10,14 +10,14 @@ const path = require('path')
 const templatesDir = path.resolve(__dirname, '../templates')
 const templateTempDir = (template) => `__temp__${template}`
 
-module.exports = function getTemplatePath(template) {
-  let thisTemplate
+module.exports = function getTemplatePath(template, isExternalTemplate) {
+	let thisTemplate;
 
-  if (template) {
-    thisTemplate = templateTempDir(template)
-  } else {
-    thisTemplate = 'standard'
-  }
+	if (isExternalTemplate) {
+		thisTemplate = templateTempDir(template);
+	} else {
+		thisTemplate = 'standard';
+	}
 
-  return path.resolve(templatesDir, thisTemplate)
-}
+	return path.resolve(templatesDir, thisTemplate);
+};
