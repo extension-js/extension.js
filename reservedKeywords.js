f5f31df7b06295372dc14a5bf45ccd6ad586bb6d
itsbrex <roach7@gmail.com>
2023-04-22 21:37:22 -0700

    Refactor reservedKeywords.js to use a more descriptive variable name and simplify the code

--- reservedKeywords.js
+++ reservedKeywords.js
@@ -1,13 +1,12 @@
 // List of words you can't use as a name for your project
 // because they conflict with either current or future program commands
-const reservedKeywords = new Set(['build', 'eject', 'start', 'test', 'open', 'deploy', 'templates', 'template', 'standard']);
 
-module.exports = function reservedKeywords() {
-	const commands = process.argv;
+const reservedKeywordSet = new Set(['build', 'eject', 'start', 'test', 'open', 'deploy', 'templates', 'template', 'standard']);
 
-	return commands.some((command) => reservedKeywords.has(command));
-};
-
-if (reservedKeywords) {
-	console.log('Reserved keywords are being used.');
+function isKeyword() {
+  const commands = process.argv;
+  
+  return commands.some((command) => reservedKeywordSet.has(command));
 }
+
+module.exports = isKeyword;
