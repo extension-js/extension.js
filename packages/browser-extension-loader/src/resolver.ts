40d3080813dc10d4a15ae99e90627c3857ae01b2
Cezar Augusto <boss@cezaraugusto.net>
2024-01-24 15:09:18 -0300

    Parse manifest pages in browser extension loader

--- packages/browser-extension-loader/src/resolver.ts
+++ packages/browser-extension-loader/src/resolver.ts
@@ -1,43 +1,68 @@
 import path from 'path'
-// import manifestFields from 'browser-extension-manifest-fields'
+import {
+  assetAsManifestAsset,
+  isManifestAsset,
+  isPagesPath,
+  isPublicPath
+} from './utils'
+
+function getPublicPath(context: string, absolutePath: string) {
+  const publicDir = path.resolve(context, 'public')
+  const relativeToPublic = path.relative(publicDir, absolutePath)
+
+  return `/public/${relativeToPublic}`
+}
+
+function getPagesPath(context: string, absolutePath: string) {
+  const pagesDir = path.resolve(context, 'pages')
+  const relativeToPages = path.relative(pagesDir, absolutePath)
+  const featureName = path.basename(relativeToPages).startsWith('index')
+    ? path.basename(path.dirname(relativeToPages))
+    : path.basename(relativeToPages)
+
+  return `/pages/${featureName}/index.html`
+}
+
+function getManifestPath(
+  context: string,
+  absolutePath: string,
+  relativePath: string
+) {
+  if (isManifestAsset(context, absolutePath)) {
+    const assetPath = assetAsManifestAsset(context, absolutePath, relativePath)
+
+    if (assetPath) {
+      return assetPath
+    }
+  }
+
+  return absolutePath
+}
 
 export default function resolvePath(
   context: string,
   relativePath: string
 ): string {
   // If URL, return as is
-  if (relativePath.startsWith('http')) {
-    return relativePath
-  }
-
-  // Build the absolute path of the public folder
-  const publicDir = path.resolve(context, 'public')
-
-  // Build the absolute path of the pages folder
-  const pagesDir = path.resolve(context, 'pages')
+  if (relativePath.startsWith('http')) return relativePath
 
   // Resolve the absolute path of the file
   const absolutePath = path.resolve(context, relativePath)
 
   // Check if the file is within the public directory
-  if (absolutePath.startsWith(publicDir)) {
-    // Return path relative to public folder
-    const relativeToPublic = path.relative(publicDir, absolutePath)
-    return `/public/${relativeToPublic}`
-  } else if (absolutePath.startsWith(pagesDir)) {
-    // Return path relative to pages folder
-    const relativeToPages = path.relative(pagesDir, absolutePath)
-    const featureName = path.basename(relativeToPages).startsWith('index')
-      ? path.basename(path.dirname(relativeToPages))
-      : path.basename(relativeToPages)
+  if (isPublicPath(context, absolutePath)) {
+    return getPublicPath(context, absolutePath)
 
-    return `/pages/${featureName}/index.html`
+    // Check if the file is within the pages directory
+  } else if (isPagesPath(context, absolutePath)) {
+    return getPagesPath(context, absolutePath)
+
+    // Check if the file is a manifest asset
   } else {
-    // TODO: Support manifest.json fields
-    // if (path_is_equal_to_some_manifest_field) {
-    // return `/[feature]/[index|script][ext]`
-    // }
+    if (isManifestAsset(context, absolutePath)) {
+      return getManifestPath(context, absolutePath, relativePath)
+    }
 
-    return absolutePath
+    return relativePath
   }
 }
