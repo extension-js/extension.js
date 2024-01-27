652b109994d2462f5989ecc4b4a310087321de1f
Cezar Augusto <boss@cezaraugusto.net>
2024-01-27 13:15:00 -0300

    Compat plugin now catches missing required manifest fields

--- packages/manifest-compat-plugin/module.ts
+++ packages/manifest-compat-plugin/module.ts
@@ -1,4 +1,4 @@
-import Ajv from 'ajv'
+import Ajv, { ErrorObject } from 'ajv'
 import fs from 'fs'
 import path from 'path'
 import {Compiler, WebpackError} from 'webpack'
@@ -14,58 +14,100 @@ interface ManifestCompatPluginOptions {
 
 export default class ManifestCompatPlugin {
   private options: ManifestCompatPluginOptions
-
+  
   constructor(options: ManifestCompatPluginOptions) {
     this.options = options
   }
+  
+  private getBrowserName () {
+    const browserName = this.options.browser
+    ? this.options.browser.substring(0, 1).toUpperCase() +
+    this.options.browser.substring(1)
+    : 'Chrome Extensions API reference'
+    
+    return browserName
+  }
+  
+  private getApiDocumentationURL (browser: string, namespace: string) {
+    const extensionKnowledge = bcd.webextensions.manifest
+    const isChrome = browser === 'chrome'
+    const chromeUrl = extensionKnowledge?.[namespace].__compat?.mdn_url
+    const mdnUrl = `https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/`
+    
+    return isChrome ? chromeUrl : mdnUrl
+  }
+
+  private getManifestDocumentationURL (browser?: string) {
+    const isChrome = browser === 'chrome'
+    const chromeUrl = 'https://developer.chrome.com/docs/extensions/reference/manifest'
+    const mdnUrl = `https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/manifest.json`
+    
+    return isChrome ? chromeUrl : mdnUrl
+  }
+
+  private missingRequiredFieldError(message: string | undefined) {
+    const hintMessage = `Update your manifest.json file to run your extension.`
+    const errorMessage = `Field \`${message}\` is required. ${hintMessage}
+
+Read more about the \`${message}\` field:
+${this.getManifestDocumentationURL(this.options.browser)}`
+    return errorMessage
+  }
+  
+  private invalidTypeFieldError(errorData: ErrorObject | undefined) {
+    const field = errorData?.instancePath.replaceAll('/', '.').slice(1) || ''
+    const message = errorData?.message
+    const namespace = field?.split('.')[0]
+    
+    return `Field ${field} ${message?.replace('be', 'be of type')}
+
+Read more about the \`${namespace}\` namespace:
+${this.getApiDocumentationURL('chrome', namespace)}`
+  }
 
   apply(compiler: Compiler) {
-    compiler.hooks.emit.tapAsync(
+    compiler.hooks.afterCompile.tapAsync(
       'CompatPlugin (module)',
       (compilation, done) => {
         const manifestPath = path.resolve(
           compiler.options.context!,
           this.options.manifestPath
-        )
-        const manifest = JSON.parse(fs.readFileSync(manifestPath, 'utf-8'))
-
-        const ajv = new Ajv()
-        addCustomFormats(ajv)
-
-        const combinedSchema = {
-          allOf: [v3Schema]
-        }
-
-        const validate = ajv.compile(combinedSchema)
-        const valid = validate(manifest)
-
-        if (!valid) {
-          const field =
-            validate.errors?.[0].instancePath.replaceAll('/', '.').slice(1) ||
-            ''
-          const extensionKnowledge = bcd.webextensions.manifest
-          const message = validate.errors?.[0].message
-          const namespace = field?.split('.')[0]
-          const chromeUrl = `https://developer.chrome.com/docs/extensions/reference/api/${namespace}`
-          const mdnUrl = extensionKnowledge?.[namespace].__compat?.mdn_url
-          const isChrome = this.options.browser === 'chrome'
-          const browserName = this.options.browser
-            ? this.options.browser.substring(0, 1).toUpperCase() +
-              this.options.browser.substring(1)
-            : 'Chrome Extensions API reference'
-
-          compilation.warnings.push(
-            new WebpackError(
-              `[manifest.json]: Manifest field ${field} ${message?.replace('be', 'be of type')}.
-
-Read more about the \`${namespace}\` namespace on ${browserName} :
-${isChrome ? chromeUrl : mdnUrl}.`
-            )
           )
-        }
+          const manifest = JSON.parse(fs.readFileSync(manifestPath, 'utf-8'))
+          
+          const ajv = new Ajv()
+          addCustomFormats(ajv)
+          
+          const combinedSchema = {
+            allOf: [v3Schema]
+          }
+          
+          const validate = ajv.compile(combinedSchema)
+          const valid = validate(manifest)
+          
+          if (!valid) {
+            const errorData = validate.errors?.[0]
+            
+            if (errorData?.keyword === 'required') {
+              const missingProperty = errorData?.params.missingProperty
+              compilation.errors.push(
+                new WebpackError(
+                  `[manifest.json]: ${this.missingRequiredFieldError(missingProperty)}`
+                  )
+                )
 
-        done()
-      }
-    )
-  }
-}
+                done()
+                return
+              }
+                
+                compilation.warnings.push(
+                  new WebpackError(`[manifest.json]: ${this.invalidTypeFieldError(errorData)}`)
+                  )
+                }
+                
+                done()
+              }
+              )
+            }
+          }
+          
