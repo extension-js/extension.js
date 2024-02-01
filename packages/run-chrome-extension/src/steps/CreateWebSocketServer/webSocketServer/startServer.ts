c4d4e1794f155e63737a93f94fa0287dcce4b42c
Cezar Augusto <boss@cezaraugusto.net>
2024-02-01 08:26:42 -0300

    Improve logging in run-chrome plugin

--- packages/run-chrome-extension/src/steps/CreateWebSocketServer/webSocketServer/startServer.ts
+++ packages/run-chrome-extension/src/steps/CreateWebSocketServer/webSocketServer/startServer.ts
@@ -1,6 +1,74 @@
 import path from 'path'
 import WebSocket from 'ws'
 import {WebpackError, type Compiler} from 'webpack'
+const {log} = console
+
+function extensionCreateServerOutput(
+  compiler: Compiler,
+  message: {
+    data?: {
+      id: string
+      manifest: Record<string, any>
+      management: Record<string, any>
+    }
+  }
+) {
+  if (!message.data) {
+    // TODO: cezaraugusto this happens when the extension
+    // can't reach the background script. This can be many
+    // things such as a mismatch config or if after an error
+    // the extension starts disabled. Improve this error.
+    throw new WebpackError(
+      '[⛔️] No data received from client. Restart the program and try again.'
+    )
+  }
+
+  const compilerOptions = compiler.options
+  const {id, manifest, management} = message.data
+
+  if (!management) {
+    if (process.env.EXTENSION_ENV === 'development') {
+      console.log('[⛔️] No management info received from client. Investigate.')
+    }
+  }
+  const {
+    name,
+    description,
+    id: parsedId,
+    version,
+    hostPermissions,
+    permissions
+  } = manifest
+
+  const manifestPath = path.join(compilerOptions.context || '', 'manifest.json')
+  const manifestFromCompiler = require(manifestPath)
+  const permissionsBefore: string[] = manifestFromCompiler.permissions || []
+  const permissionsAfter: string[] = permissions || []
+  // If a permission is used in the post compilation but not
+  // in the pre-compilation step, add a "dev only" string to it.
+  const permissionsParsed: string[] = permissionsAfter.map((permission) => {
+    if (permissionsBefore.includes(permission)) return permission
+    return `${permission} (dev only)`
+  })
+  const fixedId = manifestFromCompiler.id === id
+  const hasHost = hostPermissions && hostPermissions.length
+
+  // TODO: cezaraugusto Also interesting:
+  // log(`• Size: 1.2 MB`)
+  // log(`• Static Pages: /pages`)
+  // log(`• Static Resources: /public`)
+  // log(`• Web Accessible Resources: /web_accessible_resources`)
+
+  log('')
+  log(`• Name: ${name} (${compilerOptions.mode} mode)`)
+  description && log(`• Description: ${description}`)
+  log(`• ID: ${parsedId} (${fixedId ? 'fixed' : 'dynamic'})`)
+  log(`• Version: ${version}`)
+  hasHost && log(`• Host Permissions: ${hostPermissions.sort().join(', ')}`)
+  log(`• Permissions: ${permissionsParsed.sort().join(', ')}`)
+  log(`• Settings URL: chrome://extensions/?id=${id}\n`)
+  log(`🧩 extension-create ►►► Running a new Chrome instance. Extension ready.`)
+}
 
 export default function (compiler: Compiler, port?: number) {
   const webSocketServer = new WebSocket.Server({
@@ -27,66 +95,7 @@ export default function (compiler: Compiler, port?: number) {
 
       if (message.status === 'clientReady') {
         setTimeout(() => {
-          if (!message.data) {
-            // TODO: cezaraugusto this happens when the extension
-            // can't reach the background script. This can be many
-            // things such as a mismatch config or if after an error
-            // the extension starts disabled. Improve this error.
-            throw new WebpackError(
-              '[⛔️] No data received from client. Restart the program and try again.'
-            )
-          }
-
-          const compilerOptions = compiler.options
-          const {id, manifest, management} = message.data
-
-          if (!management) {
-            if (process.env.EXTENSION_ENV === 'development') {
-              console.log(
-                '[⛔️] No management info received from client. Investigate.'
-              )
-            }
-          }
-
-          const manifestPath = path.join(
-            compilerOptions.context || '',
-            'manifest.json'
-          )
-          const manifestFromCompiler = require(manifestPath)
-          const permissionsBefore: string[] =
-            manifestFromCompiler.permissions || []
-          const permissionsAfter: string[] = manifest.permissions || []
-          const isMutableId = manifestFromCompiler.id !== id
-
-          // If a permission is used in the post compilation but not
-          // in the pre-compilation step, add a "dev only" string to it.
-          const permissions: string[] = permissionsAfter.map((permission) => {
-            if (permissionsBefore.includes(permission)) return permission
-            return `${permission} (dev only)`
-          })
-
-          // TODO: cezaraugusto Also interesting:
-          // • Size: 1.2 MB
-          // • Static Pages: /pages
-          // • Static Resources: /public
-          // • Web Accessible Resources: /web_accessible_resources
-          console.log('')
-          console.log(`• Name: ${manifest.name} (${compilerOptions.mode} mode)`)
-          console.log(`• Description: ${manifest.description}`)
-          console.log(
-            `• ID: ${id} (${isMutableId ? 'dynamic' : 'static'})`
-          )
-          console.log(`• Version: ${manifest.version}`)
-          manifest.hostPermissions &&
-            manifest.hostPermissions.length &&
-            console.log(
-              `• Host Permissions: ${manifest.hostPermissions.join(', ')}`
-            )
-          console.log(`• Permissions: ${permissions.sort().join(', ')}`)
-          console.log(`• Settings URL: chrome://extensions/?id=${id}\n`)
-          console.log(
-            `🧩 extension-create ►►► Running a new Chrome instance. Extension ready.`
-          )
+          extensionCreateServerOutput(compiler, message)
         }, 1000)
       }
     })
