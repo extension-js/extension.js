1aa505ba8f5327939b72dccf97a29c93bee1a44e
Cezar Augusto <boss@cezaraugusto.net>
2024-02-13 16:22:59 -0300

    Update naming convention around plugin bundles

--- programs/develop/webpack/plugins/errorPlugins.ts
+++ programs/develop/webpack/plugins/errorPlugins.ts
@@ -11,20 +11,25 @@ import ManifestCompatPlugin from 'webpack-browser-extension-manifest-compat-plug
 import CommonErrorsPlugin from 'webpack-browser-extension-common-errors-plugin'
 import {type DevOptions} from '../../extensionDev'
 
-export default function errorPlugins(projectPath: string, {mode}: DevOptions) {
+export default function errorPlugins(
+  projectPath: string,
+  {mode, browser}: DevOptions
+) {
   return {
-    name: 'errorPlugins',
+    name: 'ErrorPlugin',
     apply: (compiler: webpack.Compiler) => {
       const manifestPath = path.resolve(projectPath, 'manifest.json')
 
       // TODO: combine all extension context errors into one.
       // new CombinedErrorsPlugin().apply(compiler)
+
       // TODO: Combine common config errors and output a nice error display.
       // new ErrorLayerPlugin().apply(compiler)
-      // TODO: Handle manifest compatibilities across browser vendors.
+
+      // Handle manifest compatibilities across browser vendors.
       new ManifestCompatPlugin({
         manifestPath,
-        browser: 'chrome'
+        browser
       }).apply(compiler)
 
       // Handle common user mistakes and webpack errors.
