4c20d47adfb2c38968aff05537c4422a140f1357
Cezar Augusto <boss@cezaraugusto.net>
2024-02-13 16:21:54 -0300

    Create SpecialFoldersPlugin

---
+++ programs/develop/webpack/plugins/SpecialFoldersPlugin/index.ts
@@ -0,0 +1,33 @@
+import {Compiler} from 'webpack'
+import CopyPublicFolder from './CopyPublicFolder'
+import WarnUponFolderChanges from './WarnUponFolderChanges'
+
+interface SpecialFoldersPluginOptions {
+  manifestPath: string
+}
+
+/**
+ * SpecialFoldersPlugin is responsible for handling the
+ * three types of special folders in the extension:
+ *
+ * - /pages - HTML pages not included in the manifest
+ * - /scripts - Script files not included in the manifest
+ * - /public - Static files not included in the manifest
+ */
+export default class SpecialFoldersPlugin {
+  private readonly options: SpecialFoldersPluginOptions
+
+  constructor(options: SpecialFoldersPluginOptions) {
+    this.options = options
+  }
+
+  apply(compiler: Compiler) {
+    const {manifestPath} = this.options
+
+    new CopyPublicFolder({
+      manifestPath
+    }).apply(compiler)
+
+    new WarnUponFolderChanges(manifestPath).apply(compiler)
+  }
+}
