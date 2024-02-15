cb8cf57f3b7d050459741634d9727e90d767995a
Cezar Augusto <boss@cezaraugusto.net>
2024-02-15 11:08:54 -0300

    Add util getManifestContent to LocalesPlugin
    This method is shared across plugins as a way
    to get the latest state of the manifest file and
    update stuff accordingly.

--- packages/locales-plugin/helpers/utils.ts
+++ packages/locales-plugin/helpers/utils.ts
@@ -1,5 +1,23 @@
+import {Compilation} from 'webpack'
+import {Manifest} from '../types'
+
 export function shouldExclude(path: string, ignorePatterns: string[]): boolean {
   return ignorePatterns.some((pattern) => {
     return path.includes(pattern)
   })
+}
+
+export function getManifestContent(
+  compilation: Compilation,
+  manifestPath: string
+): Manifest {
+  if (
+    compilation.getAsset('manifest.json') ||
+    compilation.assets['manifest.json']
+  ) {
+    const manifest = compilation.assets['manifest.json'].source().toString()
+    return JSON.parse(manifest || '{}')
+  }
+
+  return require(manifestPath)
 }
