4c7dfb12312527861058bf2176ea95a3496c225d
Cezar Augusto <boss@cezaraugusto.net>
2024-02-15 11:09:17 -0300

    Add util getManifestContent to ResourcesPlugin
    This method is shared across plugins as a way
    to get the latest state of the manifest file and
    update stuff accordingly.

---
+++ packages/resources-plugin/loaders/utils.ts
@@ -0,0 +1,21 @@
+import {Compilation} from 'webpack'
+import {Manifest} from '../types'
+
+function getManifestContent(
+  compilation: Compilation,
+  manifestPath: string
+): Manifest {
+  if (
+    compilation.getAsset('manifest.json') ||
+    compilation.assets['manifest.json']
+  ) {
+    const manifest = compilation.assets['manifest.json'].source().toString()
+    return JSON.parse(manifest || '{}')
+  }
+
+  return require(manifestPath)
+}
+
+export default {
+  getManifestContent
+}
