61391537f89b6754a1d1637afae7929c04004b2f
Cezar Augusto <boss@cezaraugusto.net>
2024-02-15 11:09:17 -0300

    Add util getManifestContent to ResourcesPlugin
    This method is shared across plugins as a way
    to get the latest state of the manifest file and
    update stuff accordingly.

--- packages/resources-plugin/types.ts
+++ packages/resources-plugin/types.ts
@@ -1,4 +1,8 @@
+import {ManifestBase} from './manifest-types'
+
 export interface WebResourcesPluginInterface {
   manifestPath: string
   exclude?: string[]
 }
+
+export type Manifest = ManifestBase
