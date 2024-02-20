3a10e47e36f8e3729975530b13035b27a505f460
Cezar Augusto <boss@cezaraugusto.net>
2024-02-20 12:19:13 -0300

    Build command is ready

--- programs/develop/webpack/buildWebpack.ts
+++ programs/develop/webpack/buildWebpack.ts
@@ -6,24 +6,116 @@
 // ╚═════╝ ╚══════╝  ╚═══╝  ╚══════╝╚══════╝ ╚═════╝ ╚═╝
 
 import webpack from 'webpack'
-
+import path from 'path'
+import fs from 'fs'
+import {log, error} from 'console'
+import {green, bold, red, underline} from '@colors/colors/safe'
 import compilerConfig from './webpack-config'
-import {type DevOptions} from '../extensionDev'
+import {BuildOptions} from '../extensionBuild'
+import {getOutputPath} from './config/getPath'
 
-export default function startWebpack(
+function getFileSize(fileSizeInBytes: number): string {
+  return `${(fileSizeInBytes / 1024).toFixed(2)}KB`
+}
+
+// Function to recursively print the tree structure
+function printTree(node: any, prefix = '') {
+  Object.keys(node).forEach((key, index, array) => {
+    const isLast = index === array.length - 1
+    const connector = isLast ? '└─' : '├─'
+    const sizeInKB = node[key].size ? ` (${getFileSize(node[key].size)})` : ''
+    log(`${prefix}${connector} ${bold(key)}${sizeInKB}`)
+    if (typeof node[key] === 'object' && !node[key].size) {
+      printTree(node[key], `${prefix}${isLast ? '   ' : '|  '}`)
+    }
+  })
+}
+
+function getAssetInfo(assets: any[] | undefined) {
+  log('\n')
+  assets?.forEach((asset) => {
+    const sizeInKB = getFileSize(asset.size)
+    log(
+      `• ${bold('Filename:')} ${asset.name}, ${bold('Size:')} ${sizeInKB}, ${bold('Path:')} ${underline(`${asset.name}`)}`
+    )
+  })
+}
+
+function getAssetsTree(assets: webpack.StatsAsset[] | undefined) {
+  const assetTree = {}
+
+  assets?.forEach((asset) => {
+    const paths = asset.name.split('/')
+    let currentLevel: any = assetTree
+
+    paths.forEach((part, index) => {
+      if (!currentLevel[part]) {
+        currentLevel[part] = {}
+      }
+      if (index === paths.length - 1) {
+        // Last part of the path, add size info
+        currentLevel[part] = {size: asset.size}
+      } else {
+        currentLevel = currentLevel[part]
+      }
+    })
+  })
+
+  log('.')
+  printTree(assetTree)
+}
+
+function getAssetsSize(assets: any[] | undefined) {
+  let totalSize = 0
+  assets?.forEach((asset) => {
+    totalSize += asset.size
+  })
+
+  return getFileSize(totalSize)
+}
+
+export default function buildWebpack(
   projectDir: string,
-  {browser}: DevOptions
+  {browser}: BuildOptions
 ) {
   const webpackConfig = compilerConfig(projectDir, 'production', {
     browser
   })
 
-  webpack(webpackConfig).run((err) => {
-    if (err != null) {
-      console.log(err)
+  webpack(webpackConfig).run((err, stats) => {
+    if (err) {
+      error(err.stack || err)
       process.exit(1)
     }
 
-    process.exit(0)
+    const vendor = browser || 'chrome'
+    // Convert stats object to JSON format
+    const statsJson = stats?.toJson()
+    const manifestPath = path.join(
+      getOutputPath(projectDir, browser || 'chrome'),
+      'manifest.json'
+    )
+    const manifest = JSON.parse(fs.readFileSync(manifestPath, 'utf8'))
+    const assets = statsJson?.assets
+    const heading = `Building ${bold(manifest.name)} extension using ${bold(vendor)} defaults...\n`
+    const buildTime = `\nBuild completed in ${((statsJson?.time || 0) / 1000).toFixed(2)} seconds.`
+    const buildStatus = `Build Status: ${stats?.hasErrors() ? red('Failed') : green('Success')}`
+    const version = `Version: ${manifest.version}`
+    const size = `Size: ${getAssetsSize(assets)}`
+    const ready = green(
+      'No errors or warnings found. Your extension is ready for deployment.'
+    )
+
+    log(heading)
+    getAssetsTree(assets)
+    getAssetInfo(assets)
+    log(buildTime)
+    log(buildStatus)
+    log(version)
+    log(size)
+
+    if (!stats?.hasErrors()) {
+      log(ready)
+    }
   })
 }
