6477920a1e09aec9b30011eba715daf0d99fcfaf
Cezar Augusto <boss@cezaraugusto.net>
2024-02-21 10:10:54 -0300

    Attempt to group hot files and decrease bundle size
    Since we write files to disk, at some point
    the hot-* files accumulate and increasingly
    slows down the development workflow.
    This change adds a new plugin that cleans
    the hot/ folder we just created in webpack
    config.

---
+++ programs/develop/webpack/plugins/CleanHotUpdatesPlugin.ts
@@ -0,0 +1,35 @@
+import * as fs from 'fs'
+import * as path from 'path'
+import {Compilation, Compiler} from 'webpack'
+
+export default class CleanHotUpdatesPlugin {
+  apply(compiler: Compiler): void {
+    compiler.hooks.thisCompilation.tap(
+      'CleanHotUpdatesPlugin',
+      (compilation) => {
+        compilation.hooks.processAssets.tap(
+          {
+            name: 'CleanHotUpdatesPlugin',
+            stage: Compilation.PROCESS_ASSETS_STAGE_ADDITIONS
+          },
+          () => {
+            const hotUpdatePath = path.join(
+              compilation.options.output.path || '',
+              'hot'
+            )
+
+            // Check if the hot/ directory exists and delete it
+            if (fs.existsSync(hotUpdatePath)) {
+              fs.rmSync(hotUpdatePath, {recursive: true, force: true})
+              if (process.env.EXTENSION_ENV === 'development') {
+                console.log(
+                  '[CleanHotUpdatesPlugin] Removed old hot-update files.'
+                )
+              }
+            }
+          }
+        )
+      }
+    )
+  }
+}
