d3f347d25b4dd3a909f1c66c6e75bbd09d44568b
Cezar Augusto <boss@cezaraugusto.net>
2024-02-21 10:11:19 -0300

    Fix case of manifest.json not throwing errors when HTML/JS file paths changed

--- packages/manifest-plugin/steps/ThrowIfRecompileIsNeeded.ts
+++ packages/manifest-plugin/steps/ThrowIfRecompileIsNeeded.ts
@@ -1,5 +1,5 @@
-import path from 'path'
-import webpack, {Compilation} from 'webpack'
+import fs from 'fs'
+import webpack from 'webpack'
 import manifestFields from 'browser-extension-manifest-fields'
 
 import {type ManifestPluginInterface, type Manifest} from '../types'
@@ -9,12 +9,21 @@ export default class ThrowIfRecompileIsNeeded {
   public readonly manifestPath: string
   public readonly exclude?: string[]
   private initialValues: string[] = []
-  private manifestChanged: boolean = false
 
   constructor(options: ManifestPluginInterface) {
     this.manifestPath = options.manifestPath
     this.exclude = options.exclude || []
-    this.initialValues = this.getFlattenedAssets()
+    this.initialValues = this.getFlattenedAssets(this.readManifest())
+  }
+
+  private readManifest(): Manifest {
+    try {
+      const manifestJson = fs.readFileSync(this.manifestPath, 'utf-8')
+      return JSON.parse(manifestJson)
+    } catch (error) {
+      console.error('Error reading manifest file:', error)
+      return {} as Manifest
+    }
   }
 
   private getFlattenedAssets(updatedManifest?: Manifest): string[] {
@@ -22,12 +31,11 @@ export default class ThrowIfRecompileIsNeeded {
     const scripts = manifestFields(this.manifestPath, updatedManifest).scripts
     const htmlFields = Object.values(html).map((value) => value?.html)
     const scriptFields = Object.values(scripts).flat()
-    const initialValues = [...htmlFields, ...scriptFields]
-    const values = initialValues
-      .map((value) => value || '')
-      .filter((value) => value !== '')
+    const values = [...htmlFields, ...scriptFields].filter(
+      (value) => value != null
+    )
 
-    return values
+    return values as string[]
   }
 
   public apply(compiler: webpack.Compiler): void {
@@ -35,40 +43,22 @@ export default class ThrowIfRecompileIsNeeded {
       'ManifestPlugin (ThrowIfRecompileIsNeeded)',
       (compiler, done) => {
         const files = compiler.modifiedFiles || new Set<string>()
-        const changedFile = Array.from(files)[0]
+        if (files.has(this.manifestPath)) {
+          const updatedManifest = this.readManifest()
+          const updatedValues = this.getFlattenedAssets(updatedManifest).sort()
+          const initialValues = this.initialValues.sort()
 
-        if (changedFile && path.basename(changedFile) === 'manifest.json') {
-          this.manifestChanged = true
-        }
-
-        done()
-      }
-    )
-
-    compiler.hooks.compilation.tap(
-      'ManifestPlugin (ThrowIfRecompileIsNeeded)',
-      (compilation) => {
-        compilation.hooks.processAssets.tap(
-          {
-            name: 'BrowserExtensionJsonPlugin',
-            stage: Compilation.PROCESS_ASSETS_STAGE_ADDITIONS
-          },
-          (assets) => {
-            if (this.manifestChanged) {
-              const manifestAsset = assets['manifest.json']?.source()
-              const manifest = JSON.parse(manifestAsset?.toString() || '{}')
-              const initialValues = this.initialValues.sort()
-              const updatedValues = this.getFlattenedAssets(manifest).sort()
-
-              if (initialValues.toString() !== updatedValues.toString()) {
+          if (initialValues.toString() !== updatedValues.toString()) {
+            compiler.hooks.thisCompilation.tap(
+              'ManifestPlugin (ThrowIfRecompileIsNeeded)',
+              (compilation) => {
                 const errorMessage = messages.serverRestartRequired()
-
                 compilation.errors.push(new webpack.WebpackError(errorMessage))
-                this.manifestChanged = false
               }
-            }
+            )
           }
-        )
+        }
+        done()
       }
     )
   }
