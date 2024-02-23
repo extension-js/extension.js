8c2cbf4d97570b31b66d2a7f9cda1dd4d77e15bd
Cezar Augusto <boss@cezaraugusto.net>
2024-02-23 13:25:32 -0300

    Fix create not working due to wrong template path

--- programs/create/helpers/getTemplatePath.ts
+++ programs/create/helpers/getTemplatePath.ts
@@ -9,6 +9,6 @@ import path from 'path'
 
 export default function getTemplatePath(workingDir: string, template: string) {
   const templateName = path.basename(template)
-  const templatesDir = path.resolve(__dirname, '../templates', templateName)
+  const templatesDir = path.resolve(__dirname, 'templates', templateName)
   return path.resolve(workingDir, templatesDir)
 }
