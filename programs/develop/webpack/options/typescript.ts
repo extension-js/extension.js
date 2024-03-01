ae3fc82f8e1aacdb4e7f251cf51067a2a6d79707
Cezar Augusto <boss@cezaraugusto.net>
2024-03-01 11:57:08 -0300

    Upgrate TypeScript configs

--- programs/develop/webpack/options/typescript.ts
+++ programs/develop/webpack/options/typescript.ts
@@ -20,15 +20,24 @@ export function getTypeScriptConfigFile(projectDir: string) {
 export function getTypeScriptConfigOverrides(projectDir: string, opts: any) {
   return {
     compilerOptions: {
+      moduleResolution: 'node', // Use Node's module resolution algorithm; useful if using npm packages
+      lib: ['esnext', 'dom'], // Include typings for latest ECMAScript features and DOM APIs
+      strict: true, // Enable all strict type-checking options
+      esModuleInterop: true, // Enables emit interoperability between CommonJS and ES Modules
+      allowSyntheticDefaultImports: true, // Allow default imports from modules with no default export
+      noEmitOnError: true, // Do not emit outputs if any errors were reported
+      resolveJsonModule: true, // Allow importing '.json' files
+      // TODO: cezaraugusto check
+      // isolatedModules: true, // Ensure each file can be safely transpiled without relying on other imports
       isolatedModules: false,
-      sourceMap: opts.mode === 'development',
+      sourceMap: opts.mode === 'development', // Generate source maps for debugging
       inlineSourceMap: false,
       declarationMap: false,
       incremental: true,
-      skipLibCheck: true,
+      skipLibCheck: true, // Skip type checking of all declaration files (*.d.ts)
       noEmit: true,
-      module: 'esnext',
-      target: 'es2021',
+      module: 'esnext', // Use ES modules, which are the standard in modern browsers
+      target: 'esnext', // Use the latest ECMAScript version for the target output
       allowJs: true,
       forceConsistentCasingInFileNames: true,
       jsx: isUsingReact(projectDir) ? 'react' : 'preserve',
@@ -38,9 +47,10 @@ export function getTypeScriptConfigOverrides(projectDir: string, opts: any) {
         '.cache',
         'tsbuildinfo'
       )
-    }
+    },
+    exclude: ['node_modules', 'dist']
   }
-}
+}      
 
 export function isUsingTypeScript(projectDir: string) {
   const packageJsonPath = path.join(projectDir, 'package.json')
