950a1ed8152f46689cbe3bc3899a2b66c25458b8
Cezar Augusto <boss@cezaraugusto.net>
2024-03-04 16:52:53 -0300

    Remove pacote from create program
    This was an undocumented feature of handling
    external templates. We want it in the same way
    dev and start are built, so pacote isn't much used.
    
    Remove it from now and in the future we should
    create the command to parse external extensions
    into something that extension-create can work.

--- programs/create/module.ts
+++ programs/create/module.ts
@@ -7,12 +7,12 @@
 
 import * as messages from './messages'
 import createDirectory from './steps/createDirectory'
-import importExternalTemplate from './steps/importExternalTemplate'
+// import importExternalTemplate from './steps/importExternalTemplate'
 import importLocalTemplate from './steps/importLocalTemplate'
 import writePackageJson from './steps/writePackageJson'
 import installDependencies from './steps/installDependencies'
 import abortAndClean from './steps/abortProjectAndClean'
-import isExternalTemplate from './helpers/isExternalTemplate'
+// import isExternalTemplate from './helpers/isExternalTemplate'
 import writeReadmeFile from './steps/writeReadmeFile'
 import generateExtensionTypes from './steps/generateExtensionTypes'
 import isTypeScriptTemplate from './helpers/isTypeScriptTemplate'
@@ -39,11 +39,11 @@ export default async function createExtension(
   try {
     await createDirectory(targetDir, projectName)
 
-    if (isExternalTemplate(template)) {
-      await importExternalTemplate(targetDir, projectName, template)
-    } else {
-      await importLocalTemplate(targetDir, projectName, template)
-    }
+    // if (isExternalTemplate(template)) {
+    //   await importExternalTemplate(targetDir, projectName, template)
+    // } else {
+    // }
+    await importLocalTemplate(targetDir, projectName, template)
 
     await writePackageJson(targetDir, projectName, template)
     await installDependencies(targetDir, projectName)
