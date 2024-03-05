f9efa6647ca60c0a64ae5128d0a80184fae9e2d4
Cezar Augusto <boss@cezaraugusto.net>
2024-03-05 18:55:54 -0300

    Update CSP patch across browser plugins

--- packages/run-edge-extension/steps/SetupReloadStrategy/ApplyManifestDevDefaults/patchCSP.ts
+++ packages/run-edge-extension/steps/SetupReloadStrategy/ApplyManifestDevDefaults/patchCSP.ts
@@ -5,11 +5,10 @@ export function patchV2CSP(manifest: ManifestBase) {
   let policy: string | undefined = manifest.content_security_policy
 
   if (!policy) {
-    manifest.content_security_policy =
+    return (
       "script-src 'self' 'unsafe-eval' blob: filesystem:; " +
-      "object-src 'self' blob: filesystem:; " +
-      'connect-src ws:;'
-    return manifest
+      "object-src 'self' blob: filesystem:; "
+    )
   }
 
   const csp = parse(policy)
@@ -22,18 +21,11 @@ export function patchV2CSP(manifest: ManifestBase) {
   if (!csp['script-src'].includes("'unsafe-eval'")) {
     csp['script-src'].push("'unsafe-eval'")
   }
-  if (!csp['connect-src']) {
-    csp['connect-src'] = ['ws:']
-  } else {
-    if (!csp['connect-src'].includes('ws:')) {
-      csp['connect-src'].push('ws:')
-    }
-  }
+
   for (const k in csp) {
     policy += `${k} ${csp[k].join(' ')};`
   }
 
-  // Update the CSP in the manifest
   return policy
 }
 
@@ -42,34 +34,19 @@ export function patchV3CSP(manifest: ManifestBase) {
   let policy: {extension_pages: string} | undefined =
     manifest.content_security_policy
 
-  // Check if a policy exists, if not, apply a default one
   if (!policy) {
     return {
-      extension_pages:
-        "script-src 'self'; " + "object-src 'self'; " + 'connect-src ws:;'
+      extension_pages: "script-src 'self'; " + "object-src 'self'; "
     }
   }
 
-  // Parse the CSP to a manageable format
   const csp = parse(policy.extension_pages)
   let extensionPagesPolicy = ''
 
-  // Ensure 'connect-src' is present and includes 'ws:' if not already
-  const hasConnectSrc = csp['connect-src'] && csp['connect-src'].length > 0
-  if (hasConnectSrc) {
-    if (!csp['connect-src'].includes('ws:')) {
-      csp['connect-src'].push('ws:')
-    }
-  } else {
-    csp['connect-src'] = ["'self'", 'ws:']
-  }
-
-  // Rebuild the CSP string
   for (const directive in csp) {
     extensionPagesPolicy += `${directive} ${csp[directive].join(' ')}; `
   }
 
-  // Update the manifest's CSP
   return {
     extension_pages: extensionPagesPolicy.trim()
   }
