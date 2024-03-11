4659b74759591319ad7857e11d71f118e237c166
Cezar Augusto <boss@cezaraugusto.net>
2024-03-11 15:51:32 -0300

    Update TypeScript template

--- programs/create/templates/typescript/template/background/lib.ts
+++
@@ -1,3 +0,0 @@
-export default function helloExtension() {
-  return 'typescript running on background script'
-}
---
+++ programs/create/templates/typescript/template/newtab/index.ts
@@ -0,0 +1,5 @@
+function sum(a: number, b: number) {
+  return a + b
+}
+
+console.log('Hello from newtab/index.ts!', sum(1, 23))
