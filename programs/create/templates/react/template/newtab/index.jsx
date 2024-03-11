64e450c5c1782d0e5e6434c705ccce69ff0a6093
Cezar Augusto <boss@cezaraugusto.net>
2024-03-11 15:49:14 -0300

    Update React template

--- programs/create/templates/react/template/newtab/index.jsx
+++ programs/create/templates/react/template/newtab/index.jsx
@@ -1,11 +1,12 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import PopupApp from './PopupApp'
-import './popup.css'
+import NewTabApp from './NewTabApp'
+import './styles.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
   <React.StrictMode>
-    <PopupApp />
+    <NewTabApp />
   </React.StrictMode>
 )
