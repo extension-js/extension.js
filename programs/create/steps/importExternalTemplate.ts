ef9da4eeef6f123086be479ec44c9694e7d81e1d
Cezar Augusto <boss@cezaraugusto.net>
2024-03-18 09:19:48 -0300

    Delete create from external template features
    They were not documented and we will
    inherit from what dev/start does via
    GitHub repositories.

--- programs/create/steps/importExternalTemplate.ts
+++ programs/create/steps/importExternalTemplate.ts
@@ -7,9 +7,7 @@
 
 import path from 'path'
 import fs from 'fs/promises'
-// import pacote from 'pacote'
 import {bold, blue} from '@colors/colors/safe'
-import {getExternalImportInfo} from '../helpers/getImportInfo'
 
 export default async function importExternalTemplate(
   workingDir: string,
@@ -17,19 +15,16 @@ export default async function importExternalTemplate(
   template: string
 ) {
   const projectPath = path.join(workingDir, projectName)
-  const importInfo = getExternalImportInfo(template)
   const templateName = path.basename(template)
 
   try {
     await fs.mkdir(projectPath, {recursive: true})
 
-    // const {name, version} = await pacote.manifest(importInfo)
     console.log(
       `🧰 - Installing ${bold(projectName)} from template ${blue(
         bold(templateName)
       )}`
     )
-    // await pacote.extract(`${name}@${version}`, projectPath)
   } catch (error: any) {
     console.error(
       `😕❓ Can't find template ${blue(bold(templateName))}. ${error}`
