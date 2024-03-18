18ab3270ae72806dd67168a5e1057d5b6f17c486
Cezar Augusto <boss@cezaraugusto.net>
2024-03-18 11:37:16 -0300

    Fix path of extension types

--- programs/create/steps/generateExtensionTypes.ts
+++ programs/create/steps/generateExtensionTypes.ts
@@ -17,7 +17,7 @@ export default async function generateExtensionTypes(
   const typePath =
     process.env.EXTENSION_ENV === 'development'
       ? path.resolve(process.cwd(), 'programs/develop/types')
-      : 'extension-create/develop/types'
+      : '@extension-create/develop/dist/types'
 
   const fileContent = `\
 // Required extension-create types for TypeScript projects.
