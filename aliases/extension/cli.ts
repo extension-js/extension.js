b726d7ffd817eb9b202cf54bce4a48b2e80c6e00
Cezar Augusto <boss@cezaraugusto.net>
2024-03-18 15:32:28 -0300

    Create "extension" alias

---
+++ aliases/extension/cli.ts
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+// ███████╗██╗  ██╗████████╗███████╗███╗   ██╗███████╗██╗ ██████╗ ███╗   ██╗
+// ██╔════╝╚██╗██╔╝╚══██╔══╝██╔════╝████╗  ██║██╔════╝██║██╔═══██╗████╗  ██║
+// █████╗   ╚███╔╝    ██║   █████╗  ██╔██╗ ██║███████╗██║██║   ██║██╔██╗ ██║
+// ██╔══╝   ██╔██╗    ██║   ██╔══╝  ██║╚██╗██║╚════██║██║██║   ██║██║╚██╗██║
+// ███████╗██╔╝ ██╗   ██║   ███████╗██║ ╚████║███████║██║╚██████╔╝██║ ╚████║
+// ╚══════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═══╝╚══════╝╚═╝ ╚═════╝ ╚═╝  ╚═══╝
+
+import {exec} from 'child_process'
+
+// Function to execute the CLI
+function runCli() {
+  // Replace `@extension-create/cli` with the actual command you want to run
+  exec('@extension-create/cli', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`)
+      return
+    }
+    console.log(`stdout: ${stdout}`)
+    console.error(`stderr: ${stderr}`)
+  })
+}
+
+runCli()
