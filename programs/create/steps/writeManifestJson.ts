8a45d71e7fc9e1a32d73651d7174f4cc632337a7
Cezar Augusto <boss@cezaraugusto.net>
2024-03-18 11:36:58 -0300

    Override manifest.json name
    - New extensions should inherit the
    name applied by the user.

---
+++ programs/create/steps/writeManifestJson.ts
@@ -0,0 +1,42 @@
+//  ██████╗██████╗ ███████╗ █████╗ ████████╗███████╗
+// ██╔════╝██╔══██╗██╔════╝██╔══██╗╚══██╔══╝██╔════╝
+// ██║     ██████╔╝█████╗  ███████║   ██║   █████╗
+// ██║     ██╔══██╗██╔══╝  ██╔══██║   ██║   ██╔══╝
+// ╚██████╗██║  ██║███████╗██║  ██║   ██║   ███████╗
+//  ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝
+
+import path from 'path'
+import fs from 'fs/promises'
+import {bold, red, yellow} from '@colors/colors/safe'
+
+export default async function writeManifestJson(
+  projectPath: string,
+  projectName: string,
+  template: string
+) {
+  const manifestJsonPath = path.join(projectPath, 'manifest.json')
+
+  const manifestJsonContent = await fs.readFile(manifestJsonPath)
+  const manifestJson = JSON.parse(manifestJsonContent.toString())
+
+  const manifestMetadata = {
+    ...manifestJson,
+    name: path.basename(projectPath),
+  }
+
+  try {
+    console.log(`📜 - Writing ${yellow(`manifest.json`)} metadata...`)
+    await fs.writeFile(
+      path.join(projectPath, 'manifest.json'),
+      JSON.stringify(manifestMetadata, null, 2)
+    )
+  } catch (error: any) {
+    console.error(
+      `🧩 ${bold(`extension-create`)} ${red(`✖︎✖︎✖︎`)} Can't write ${yellow(
+        `manifest.json`
+      )} for ${bold(projectName)}. ${error}`
+    )
+
+    process.exit(1)
+  }
+}
