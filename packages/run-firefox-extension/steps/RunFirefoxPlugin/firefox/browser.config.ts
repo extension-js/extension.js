12c3f3d0528ec6d5081db9e42f8c7c6fe3ba2359
Cezar Augusto <boss@cezaraugusto.net>
2024-05-03 14:44:47 -0300

    Add boilerplate for upcoming Firefox support

--- packages/run-firefox-extension/steps/RunFirefoxPlugin/firefox/browser.config.ts
+++ packages/run-firefox-extension/steps/RunFirefoxPlugin/firefox/browser.config.ts
@@ -1,4 +1,5 @@
 import path from 'path'
+import fs from 'fs'
 import {type RunFirefoxExtensionInterface} from '../../../types'
 import createUserDataDir from './createUserDataDir'
 
@@ -13,30 +14,64 @@ const reloadExtension = path.resolve(
   'reload-extension'
 )
 
+function getManifestId(extensionPath: string) {
+  const manifestPath = path.join(extensionPath, 'manifest.json')
+  const manifest = require(manifestPath)
+
+  return manifest?.id || manifest?.browser_specific_settings?.gecko?.id
+}
+
+function copyDirectory(srcDir: string, destDir: string) {
+  if (!fs.existsSync(destDir)) {
+    fs.mkdirSync(destDir, {recursive: true})
+  }
+
+  const items = fs.readdirSync(srcDir, {withFileTypes: true})
+
+  items.forEach((item) => {
+    const srcPath = path.join(srcDir, item.name)
+    const destPath = path.join(destDir, item.name)
+
+    if (item.isDirectory()) {
+      copyDirectory(srcPath, destPath)
+    } else {
+      fs.copyFileSync(srcPath, destPath)
+    }
+  })
+}
+
 export default function browserConfig(
   configOptions: RunFirefoxExtensionInterface
 ) {
-  const userBrowserExtension = configOptions.extensionPath?.replace(
-    'manifest.json',
-    ''
-  )
+  const userBrowserExtension =
+    path.dirname(configOptions.manifestPath as string) + '/dist/firefox'
 
-  const extensionsToLoad = [`"${userBrowserExtension}"`, managerExtension]
+  // const extensionsToLoad: string[] = []
+  const extensionsToLoad: string[] = [
+    `"${userBrowserExtension}"`,
+    managerExtension
+  ]
 
   if (configOptions.autoReload) {
     extensionsToLoad.push(reloadExtension)
   }
 
   const profile = createUserDataDir(configOptions.userDataDir)
+  // extensionsDir exists but is not typed in the library.
+  const extensionsDir = (profile as any).extensionsDir
 
-  profile.addExtensions(extensionsToLoad, function () {})
+  for (const extensionPath of extensionsToLoad) {
+    const extensionPathNormalized = extensionPath.replace(/"/g, '')
+    const extensionId = getManifestId(extensionPathNormalized)
 
-  // Flags set by default:
-  // https://github.com/GoogleFirefox/firefox-launcher/blob/master/src/flags.ts
-  return [
-    `--user-data-dir=${profile.path()}`,
-    // Flags to pass to Firefox
-    // Any of http://peter.sh/experiments/chromium-command-line-switches/
-    ...(configOptions.browserFlags || [])
-  ].join(' ')
+    const destPath = path.join(extensionsDir, `${extensionId}`)
+
+    // BUG: cezaraugusto for some reason directory
+    // is copied but then removed from the correct path.
+    copyDirectory(extensionPathNormalized, destPath)
+  }
+
+  // Assume updatePreferences is a method of profile that needs to be called.
+  profile.updatePreferences()
+  return profile
 }
