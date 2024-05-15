f5aeb511dcaff6996d9e14169de835d4e76f2c97
fe-xiaoxin <zxin088@gmail.com>
2024-05-15 15:30:45 +0800

    feat: Add vue template

---
+++ programs/create/templates/vue/template/newtab/NewTabApp.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  data() {
+    return {
+      message: 'Welcome to your Vue Extension.'
+    }
+  }
+}
+</script>
+<template>
+  <header>
+    <h1>
+      <img
+        class="vue"
+        src="../public/vue.svg"
+        alt="The Vue logo"
+        width="120px"
+      />
+      <br />
+      {{message}}
+    </h1>
+    <p>
+      Learn more about creating browser extensions at 
+      <a href="https://extension.js.org" target="_blank">
+        https://extension.js.org
+      </a>
+      .
+    </p>
+  </header>
+</template>
