46cc8105a3d1d8a3f4811eb57dad2f6c78cb0d8b
小鑫同学 <zxin088@gmail.com>
2024-05-19 09:53:51 +0800

    Add vue-loader and template support (#61)
    * feat: Add vuejs support
    * feat: Add vue template
    * feat: Add vue-typescript template

--- programs/develop/webpack/loaders/jsLoaders.ts
+++ programs/develop/webpack/loaders/jsLoaders.ts
@@ -7,6 +7,34 @@
 
 import {babelConfig} from '../options/babel'
 import {isUsingTypeScript} from '../options/typescript'
+import {isUsingVue} from '../options/vue'
+
+type Loader = {
+  [index: string]: any
+}
+
+const vueLoaders = (projectDir: string): Loader[] => {
+  const vueLoaders: Loader[] = [
+    {
+      test: /\.vue$/,
+      loader: require.resolve('vue-loader')
+    }
+  ]
+
+  // use vue and typescript, need to add ts-loader
+  if (isUsingTypeScript(projectDir)) {
+    vueLoaders.push({
+      test: /\.ts?$/,
+      loader: require.resolve('ts-loader'),
+      options: {
+        appendTsSuffixTo: [/\.vue$/],
+        // Skip type checking
+        transpileOnly: true
+      }
+    })
+  }
+  return vueLoaders
+}
 
 export default function jsLoaders(projectDir: string, opts: any) {
   // Prevent users from running ts/tsx files when not using TypeScript
@@ -14,7 +42,7 @@ export default function jsLoaders(projectDir: string, opts: any) {
     ? /\.(js|mjs|jsx|mjsx|ts|mts|tsx|mtsx)$/
     : /\.(js|mjs|jsx|mjsx)$/
 
-  const jsLoaders = [
+  const jsLoaders: Loader[] = [
     // https://webpack.js.org/loaders/babel-loader/
     // https://babeljs.io/docs/en/babel-loader
     {
@@ -28,6 +56,9 @@ export default function jsLoaders(projectDir: string, opts: any) {
       })
     }
   ]
+
+  // Add vue-loader when using vue
+  isUsingVue(projectDir) && jsLoaders.push(...vueLoaders(projectDir))
 
   return jsLoaders
 }
