50ed3d8f121200f42f6f199338cf1128d34168ad
小鑫同学 <zxin088@gmail.com>
2024-05-19 09:53:51 +0800

    Add vue-loader and template support (#61)
    * feat: Add vuejs support
    * feat: Add vue template
    * feat: Add vue-typescript template

---
+++ programs/create/templates/vue-typescript/template/content/content.ts
@@ -0,0 +1,15 @@
+import './base.css'
+import './content.css'
+
+import {createApp} from 'vue'
+import ContentApp from './ContentApp.vue'
+
+function initial() {
+  const rootDiv = document.createElement('div')
+  rootDiv.id = 'extension-root'
+  document.body.appendChild(rootDiv)
+
+  createApp(ContentApp).mount(rootDiv)
+}
+
+setTimeout(initial, 1000)
