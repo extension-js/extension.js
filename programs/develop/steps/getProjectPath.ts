aa5eb94c1e32fd7bfd598c8cb196b152810372fc
Cezar Augusto <boss@cezaraugusto.net>
2024-06-05 10:53:51 -0300

    Fix all weakness found by CodeQL scan

--- programs/develop/steps/getProjectPath.ts
+++ programs/develop/steps/getProjectPath.ts
@@ -10,6 +10,15 @@ import goGitIt from 'go-git-it'
 import {blue, green, white, bold, underline} from '@colors/colors/safe'
 import downloadAndExtractZip from './extractFromZip'
 
+const isUrl = (url: string) => {
+  try {
+    new URL(url)
+    return true
+  } catch (e) {
+    return false
+  }
+}
+
 async function importUrlSourceFromGithub(
   pathOrRemoteUrl: string,
   text: string
@@ -34,38 +43,42 @@ export default async function getProjectPath(
     return process.cwd()
   }
 
-  if (pathOrRemoteUrl.startsWith('http')) {
-    if (!pathOrRemoteUrl.startsWith('https://github.com')) {
-      const urlSource = await importUrlSourceFromZip(pathOrRemoteUrl)
+  if (isUrl(pathOrRemoteUrl)) {
+    const url = new URL(pathOrRemoteUrl)
+
+    if (url.protocol.startsWith('http')) {
+      if (url.origin !== 'https://github.com') {
+        const urlSource = await importUrlSourceFromZip(pathOrRemoteUrl)
+
+        return urlSource
+      }
+
+      const urlData = url.pathname.split('/')
+      const owner = urlData.slice(1, 3)[0]
+      const project = urlData.slice(1, 3)[1]
+      const projectName = path.basename(url.pathname)
+      console.log(
+        `🧩 ${bold(`Extension.js`)} ${green(`►►►`)} Fetching data from ${blue(
+          underline(`https://github.com/${owner}/${project}`)
+        )}`
+      )
+      const downloadingText = `🧩 ${bold(`Extension.js`)} ${green(
+        `►►►`
+      )} Downloading ${bold(projectName)}...`
+      const urlSource = await importUrlSourceFromGithub(
+        pathOrRemoteUrl,
+        downloadingText
+      )
+      console.log(
+        `🧩 ${bold(`Extension.js`)} ${green(
+          `►►►`
+        )} Creating a new browser extension in ${white(
+          underline(`${process.cwd()}/${projectName}`)
+        )}`
+      )
 
       return urlSource
     }
-
-    const urlData = new URL(pathOrRemoteUrl).pathname.split('/')
-    const owner = urlData.slice(1, 3)[0]
-    const project = urlData.slice(1, 3)[1]
-    const projectName = path.basename(pathOrRemoteUrl)
-    console.log(
-      `🧩 ${bold(`Extension.js`)} ${green(`►►►`)} Fetching data from ${blue(
-        underline(`https://github.com/${owner}/${project}`)
-      )}`
-    )
-    const downloadingText = `🧩 ${bold(`Extension.js`)} ${green(
-      `►►►`
-    )} Downloading ${bold(projectName)}...`
-    const urlSource = await importUrlSourceFromGithub(
-      pathOrRemoteUrl,
-      downloadingText
-    )
-    console.log(
-      `🧩 ${bold(`Extension.js`)} ${green(
-        `►►►`
-      )} Creating a new browser extension in ${white(
-        underline(`${process.cwd()}/${projectName}`)
-      )}`
-    )
-
-    return urlSource
   }
 
   return path.resolve(process.cwd(), pathOrRemoteUrl)
