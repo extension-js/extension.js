da612a255bbfbfd26d0b8e26e64d8862b742c470
小鑫同学 <zxin088@gmail.com>
2024-06-06 21:07:28 +0800

    feat: support preact (#75)

---
+++ programs/create/templates/preact/template/newtab/NewTabApp.jsx
@@ -0,0 +1,25 @@
+import {Component} from 'preact'
+import 'sakura.css'
+import './base.css'
+import Logo from './logo'
+
+export default class NewTabApp extends Component {
+  render() {
+    return (
+      <header>
+        <h1>
+          <Logo height="120px" title="The Preact logo" inverted />
+          <br />
+          Welcome to your Preact Extension.
+        </h1>
+        <p>
+          Learn more about creating browser extensions at{' '}
+          <a href="https://extension.js.org" target="_blank">
+            https://extension.js.org
+          </a>
+          .
+        </p>
+      </header>
+    )
+  }
+}
