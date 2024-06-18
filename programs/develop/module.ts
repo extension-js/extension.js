e34495f9f8907e3129a5da60faf4bcd3ed0d6554
Cezar Augusto <boss@cezaraugusto.net>
2024-06-17 22:34:48 -0300

    Add `preview` command

--- programs/develop/module.ts
+++ programs/develop/module.ts
@@ -8,5 +8,6 @@
 import extensionDev from './extensionDev'
 import extensionStart from './extensionStart'
 import extensionBuild from './extensionBuild'
+import extensionPreview from './extensionPreview'
 
-export {extensionStart, extensionDev, extensionBuild}
+export {extensionStart, extensionDev, extensionBuild, extensionPreview}
