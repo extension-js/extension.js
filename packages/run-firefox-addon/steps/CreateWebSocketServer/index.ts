ebad5ae4a88fd129d36133ca7f2a96bfd4307f77
Cezar Augusto <boss@cezaraugusto.net>
2024-06-18 00:37:25 -0300

    Fix Firefox not showing output message consistently
    We hardcode the output of the Firefox runtime
    as we need manual user action to create
    a secure server for the reload strategy.

--- packages/run-firefox-addon/steps/CreateWebSocketServer/index.ts
+++ packages/run-firefox-addon/steps/CreateWebSocketServer/index.ts
@@ -5,7 +5,6 @@ import {type ManifestBase} from '../../manifest-types'
 import messageDispatcher from './webSocketServer/messageDispatcher'
 import startServer from './webSocketServer/startServer'
 import rewriteReloadPort from './rewriteReloadPort'
-import rewriteFirstRunVariable from './rewriteFirstRunVariable'
 import messages from '../../helpers/messages'
 import isFirstRun from '../RunFirefoxPlugin/firefox/isFirstRun'
 
@@ -22,56 +21,6 @@ export default class CreateWebSocketServer {
     // Before all, rewrite the reload service file
     // with the user-provided port.
     rewriteReloadPort(this.options.port || 8002)
-
-    // And also rewrite the first run variable.
-    // This will change the user active tab on first run.
-    rewriteFirstRunVariable()
-
-    // The initial stdout of this plugin where the extension info is provided
-    // is handled by the reload service. However Firefox requires a secure connection
-    // to authorize the websocket message passing. In this case, show a hardcoded
-    // message with the extension data for the first run, and inform users that they
-    // can run a command like `npx mkcert-cli` to create a certificate for the extension.
-    if (isFirstRun()) {
-      const manifest: ManifestBase = require(this.options.manifestPath)
-
-      setTimeout(() => {
-        const hardcodedMessage = {
-          data: {
-            id: manifest.browser_specific_settings?.gecko?.id,
-            manifest,
-            management: {
-              id: manifest.browser_specific_settings?.gecko?.id,
-              mayDisable: true,
-              optionsUrl: '',
-              installType: 'development' as 'development',
-              type: 'extension' as 'extension',
-              enabled: true,
-              name: manifest.name,
-              description: manifest.description || '',
-              version: manifest.version,
-              hostPermissions: manifest.host_permissions,
-              permissions: manifest.permissions
-            }
-          }
-        }
-
-        if (this.options.stats === true) {
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-          messages.extensionData(compiler, hardcodedMessage, true)
-        }
-
-        messages.stdoutData(compiler, hardcodedMessage)
-
-        if (this.options.stats === true) {
-          // Add a delay to ensure the message is sent
-          // after other runner messages.
-          setTimeout(() => {
-            messages.isFirstRun()
-          }, 2500)
-        }
-      }, 6000)
-    }
 
     // Start webSocket server to communicate with the extension.
     const wss = startServer(compiler, this.options)
