5ba92747a636f17028e5841fd8e10e6c7a3ca652
Cezar Augusto <boss@cezaraugusto.net>
2024-06-20 16:34:37 -0300

    Add tests for the CLI
    - Removed minimal Node version to run

--- programs/cli/spec/cli.spec.ts
+++ programs/cli/spec/cli.spec.ts
@@ -9,11 +9,10 @@ import path from 'path'
 import fs from 'fs'
 import {exec} from 'child_process'
 import {promisify} from 'util'
-import mockFs from 'mock-fs'
 
 const execAsync = promisify(exec)
 
-async function executeCLI(command: string = '') {
+async function extensionProgram(command: string = '') {
   const cliCommand = `ts-node ${path.join(
     __dirname,
     '..',
@@ -30,164 +29,135 @@ async function removeDir(dirPath: string) {
 }
 
 describe('CLI Commands', () => {
-  describe.skip('The CLI itself', () => {
-    const extensionPath = path.join(__dirname, '..', 'my-extension')
-    const customPath = path.join(__dirname, '..', 'my-custom-path')
-
-    beforeEach(async () => {
-      await removeDir(extensionPath)
-      await removeDir(customPath)
-      mockFs.restore()
-    })
-
-    afterAll(async () => {
-      await removeDir(extensionPath)
-      await removeDir(customPath)
-    })
-
-    it('throws an error if no command is provided', async () => {
+  describe('The CLI itself', () => {
+    it('returns usage instructions if no command is provided', async () => {
       try {
-        await executeCLI()
+        await extensionProgram()
       } catch (error: any) {
         expect(error).toBeTruthy()
-        expect(error.message).toContain(
-          "missing required argument 'project-name|project-path"
-        )
+        expect(error.message).toContain('Usage: extension [options] [command]')
       }
-    })
-
-    it('should exit if Node version is less than 18', async () => {
-      Object.defineProperty(process, 'version', {value: 'v17.0.0'})
-
-      try {
-        await executeCLI('create my-extension')
-      } catch (error: any) {
-        expect(error).toBeTruthy()
-        expect(error.message).toContain('unsupportedNodeVersion')
-      }
-
-      Object.defineProperty(process, 'version', {value: 'v18.0.0'})
     })
 
     it('should display the help message', async () => {
-      const {stdout} = await executeCLI('--help')
+      const {stdout} = await extensionProgram('--help')
       expect(stdout).toContain('Usage:')
     })
   })
 
-  describe.skip('Create Command', () => {
-    const extensionPath = path.join(__dirname, '..', 'my-extension')
-    const customPath = path.join(__dirname, '..', 'my-custom-path')
+  // describe.skip('Create Command', () => {
+  //   const extensionPath = path.join(__dirname, '..', 'my-extension')
+  //   const customPath = path.join(__dirname, '..', 'my-custom-path')
 
-    beforeEach(async () => {
-      await removeDir(extensionPath)
-      await removeDir(customPath)
-      mockFs.restore()
-    })
+  //   beforeEach(async () => {
+  //     await removeDir(extensionPath)
+  //     await removeDir(customPath)
+  //     mockFs.restore()
+  //   })
 
-    afterAll(async () => {
-      await removeDir(extensionPath)
-      await removeDir(customPath)
-    })
+  //   afterAll(async () => {
+  //     await removeDir(extensionPath)
+  //     await removeDir(customPath)
+  //   })
 
-    it('throws an error if target directory has conflicting files', async () => {
-      mockFs({
-        'my-extension': {
-          'package.json': 'some content'
-        }
-      })
+  //   it('throws an error if target directory has conflicting files', async () => {
+  //     mockFs({
+  //       'my-extension': {
+  //         'package.json': 'some content'
+  //       }
+  //     })
 
-      try {
-        await executeCLI('create my-extension')
-      } catch (error: any) {
-        expect(error).toBeTruthy()
-        expect(error.message).toContain('includes conflicting files')
-      }
-    })
+  //     try {
+  //       await extensionProgram('create my-extension')
+  //     } catch (error: any) {
+  //       expect(error).toBeTruthy()
+  //       expect(error.message).toContain('includes conflicting files')
+  //     }
+  //   })
 
-    it('throws an error if no project name is provided', async () => {
-      try {
-        await executeCLI('create')
-      } catch (error: any) {
-        expect(error).toBeTruthy()
-        expect(error.message).toContain(
-          "missing required argument 'project-name|project-path"
-        )
-      }
-    })
+  //   it('throws an error if no project name is provided', async () => {
+  //     try {
+  //       await extensionProgram('create')
+  //     } catch (error: any) {
+  //       expect(error).toBeTruthy()
+  //       expect(error.message).toContain(
+  //         "missing required argument 'project-name|project-path"
+  //       )
+  //     }
+  //   })
 
-    it('creates a new extension', async () => {
-      const extensionPath = path.join(__dirname, '..', 'my-extension')
-      await executeCLI('create my-extension')
-      expect(fs.existsSync(extensionPath)).toBeTruthy()
-    })
+  //   it('creates a new extension', async () => {
+  //     const extensionPath = path.join(__dirname, '..', 'my-extension')
+  //     await extensionProgram('create my-extension')
+  //     expect(fs.existsSync(extensionPath)).toBeTruthy()
+  //   })
 
-    it('creates a new extension from a react template', async () => {
-      const extensionPath = path.join(__dirname, '..', 'my-extension')
-      await executeCLI('create my-extension --template react')
-      expect(fs.existsSync(extensionPath)).toBeTruthy()
-    })
+  //   it('creates a new extension from a react template', async () => {
+  //     const extensionPath = path.join(__dirname, '..', 'my-extension')
+  //     await extensionProgram('create my-extension --template react')
+  //     expect(fs.existsSync(extensionPath)).toBeTruthy()
+  //   })
 
-    it('creates a new extension from a typescript template', async () => {
-      const extensionPath = path.join(__dirname, '..', 'my-extension')
-      await executeCLI('create my-extension --template typescript')
-      expect(fs.existsSync(extensionPath)).toBeTruthy()
-    })
+  //   it('creates a new extension from a typescript template', async () => {
+  //     const extensionPath = path.join(__dirname, '..', 'my-extension')
+  //     await extensionProgram('create my-extension --template typescript')
+  //     expect(fs.existsSync(extensionPath)).toBeTruthy()
+  //   })
 
-    it('creates a new extension from a react-typescript template', async () => {
-      const extensionPath = path.join(__dirname, '..', 'my-extension')
-      await executeCLI('create my-extension --template react-typescript')
-      expect(fs.existsSync(extensionPath)).toBeTruthy()
-    })
+  //   it('creates a new extension from a react-typescript template', async () => {
+  //     const extensionPath = path.join(__dirname, '..', 'my-extension')
+  //     await extensionProgram('create my-extension --template react-typescript')
+  //     expect(fs.existsSync(extensionPath)).toBeTruthy()
+  //   })
 
-    it('creates a new extension in a custom output directory', async () => {
-      const extensionPath = path.join(__dirname, '..', 'my-custom-path')
-      await executeCLI('create my-custom-path/my-extension')
-      expect(fs.existsSync(extensionPath)).toBeTruthy()
-    })
+  //   it('creates a new extension in a custom output directory', async () => {
+  //     const extensionPath = path.join(__dirname, '..', 'my-custom-path')
+  //     await extensionProgram('create my-custom-path/my-extension')
+  //     expect(fs.existsSync(extensionPath)).toBeTruthy()
+  //   })
 
-    it('creates a new extension from a template in a custom output directory.', async () => {
-      const extensionPath = path.join(__dirname, '..', 'my-custom-path')
-      await executeCLI('create my-custom-path/my-extension --template react')
-      expect(fs.existsSync(extensionPath)).toBeTruthy()
-    })
-  })
+  //   it('creates a new extension from a template in a custom output directory.', async () => {
+  //     const extensionPath = path.join(__dirname, '..', 'my-custom-path')
+  //     await extensionProgram('create my-custom-path/my-extension --template react')
+  //     expect(fs.existsSync(extensionPath)).toBeTruthy()
+  //   })
+  // })
 
-  describe.skip('Dev Command', () => {
-    /**
-     * can develop an extension
-     * can develop an extension from a remote url
-     * can start an extension in a custom output directory
-     * can launch using chrome
-     * can launch using edge
-     * can launch using firefox
-     * can launch with a custom user data dir
-     * can launch without a polyfill (TODO)
-     * can launch with a custom port
-     */
-  })
+  // describe.skip('Dev Command', () => {
+  //   /**
+  //    * can develop an extension
+  //    * can develop an extension from a remote url
+  //    * can start an extension in a custom output directory
+  //    * can launch using chrome
+  //    * can launch using edge
+  //    * can launch using firefox
+  //    * can launch with a custom user data dir
+  //    * can launch without a polyfill (TODO)
+  //    * can launch with a custom port
+  //    */
+  // })
 
-  describe.skip('Start Command', () => {
-    /**
-     * can start an extension
-     * can start an extension from a remote url
-     * can start an extension in a custom output directory
-     * can launch using chrome
-     * can launch using edge
-     * can launch using firefox
-     * can launch with a custom user data dir
-     * can launch without a polyfill
-     * can launch with a custom port
-     */
-  })
+  // describe.skip('Start Command', () => {
+  //   /**
+  //    * can start an extension
+  //    * can start an extension from a remote url
+  //    * can start an extension in a custom output directory
+  //    * can launch using chrome
+  //    * can launch using edge
+  //    * can launch using firefox
+  //    * can launch with a custom user data dir
+  //    * can launch without a polyfill
+  //    * can launch with a custom port
+  //    */
+  // })
 
-  describe.skip('Build Command', () => {
-    /**
-     * can build an extension
-     * can build an extension with chrome
-     * can build an extension with edge
-     * can build an extension with firefox
-     * can build without a polyfill
-     */
-  })
+  // describe.skip('Build Command', () => {
+  //   /**
+  //    * can build an extension
+  //    * can build an extension with chrome
+  //    * can build an extension with edge
+  //    * can build an extension with firefox
+  //    * can build without a polyfill
+  //    */
+  // })
 })
