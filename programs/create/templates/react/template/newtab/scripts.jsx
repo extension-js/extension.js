02a332a7a2b494e9fb0398b33a72282f5cdbf7f9
小鑫同学 <zxin088@gmail.com>
2024-06-27 21:07:15 +0800

    Fixed import component name (#113)

--- programs/create/templates/react/template/newtab/scripts.jsx
+++ programs/create/templates/react/template/newtab/scripts.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import NewtabApp from './NewtabApp'
+import NewtabApp from './NewTabApp'
 import './styles.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
