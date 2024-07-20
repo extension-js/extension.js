4fb6fc7702a690eb81c770a8d8b1e6389860b6d9
Cezar Augusto <boss@cezaraugusto.net>
2024-07-20 16:11:31 -0300

    Remove old intances of browser/reload plugins

---
+++ programs/develop/webpack/plugin-browsers/types.ts
@@ -0,0 +1,12 @@
+export interface PluginInterface extends PluginOptions {
+  browser?: string;
+  extension: string | string[];
+}
+
+export interface PluginOptions {
+  browserFlags?: string[];
+  userDataDir?: string;
+  profile?: string;
+  preferences?: string;
+  startingUrl?: string;
+}
