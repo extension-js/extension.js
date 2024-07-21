c07487ff0788c622e423505b423b3241066c1702
Cezar Augusto <boss@cezaraugusto.net>
2024-07-21 00:22:22 -0300

    Update compilation plugins

--- programs/develop/webpack/plugin-compilation/clean-hot-updates.ts
+++ programs/develop/webpack/plugin-compilation/clean-hot-updates.ts
@@ -2,7 +2,7 @@ import fs from 'fs'
 import path from 'path'
 import {type Compiler} from 'webpack'
 
-export default class CleanHotUpdatesPlugin {
+export class CleanHotUpdatesPlugin {
   apply(compiler: Compiler): void {
     const hotUpdatePath = path.join(compiler.options.output.path || '', 'hot')
 
--- programs/develop/webpack/plugin-errors/common-errors/index.ts
+++ programs/develop/webpack/plugin-errors/common-errors/index.ts
@@ -1,16 +1,16 @@
-import type webpack from 'webpack';
+import type webpack from 'webpack'
 import {
   handleMultipleAssetsError,
   handleTopLevelAwaitError,
-  handleCantResolveError,
-} from './steps/compilation-error-handlers';
-import { type PluginInterface } from '../../../types';
+  handleCantResolveError
+} from './steps/compilation-error-handlers'
+import {type PluginInterface} from '../../../types'
 
 export class CommonErrorsPlugin {
-  public readonly manifestPath: string;
+  public readonly manifestPath: string
 
   constructor(options: PluginInterface) {
-    this.manifestPath = options.manifestPath;
+    this.manifestPath = options.manifestPath
   }
 
   apply(compiler: webpack.Compiler) {
@@ -23,31 +23,31 @@ export class CommonErrorsPlugin {
           const multipleAssetsError = handleMultipleAssetsError(
             this.manifestPath,
             error
-          );
+          )
           const topLevelAwaitError = handleTopLevelAwaitError(
             this.manifestPath,
             error
-          );
+          )
           const cantResolveError = handleCantResolveError(
             this.manifestPath,
             error
-          );
+          )
 
           if (multipleAssetsError) {
-            compilation.errors[index] = multipleAssetsError;
+            compilation.errors[index] = multipleAssetsError
           }
 
           if (topLevelAwaitError) {
-            compilation.errors[index] = topLevelAwaitError;
+            compilation.errors[index] = topLevelAwaitError
           }
 
           if (cantResolveError) {
-            compilation.errors[index] = cantResolveError;
+            compilation.errors[index] = cantResolveError
           }
-        });
+        })
 
-        done();
-      });
-    });
+        done()
+      })
+    })
   }
 }
