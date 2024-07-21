bbe125d8f19b14ef1f2f1a22ddb9c6bbf4c5b6c3
Cezar Augusto <boss@cezaraugusto.net>
2024-07-21 00:22:22 -0300

    Update compilation plugins

---
+++ programs/develop/webpack/plugin-compilation/env.ts
@@ -0,0 +1,58 @@
+import fs from 'fs'
+import path from 'path'
+import colors, {bold, blue, yellow} from '@colors/colors/safe'
+import Dotenv from 'dotenv-webpack'
+import {type Compiler} from 'webpack'
+import {type Manifest} from '../../types'
+
+export class EnvPlugin {
+  public readonly manifestPath: string
+
+  constructor(options: {manifestPath: string}) {
+    this.manifestPath = options.manifestPath
+  }
+
+  apply(compiler: Compiler) {
+    const manifest: Manifest = require(this.manifestPath)
+    const manifestName = manifest.name
+    const manifestVersion = manifest.version
+
+    // Writes the project name and version to the terminal
+    compiler.hooks.done.tap('BoringPlugin', (stats) => {
+      const divider = stats.hasErrors()
+        ? colors.red('✖︎✖︎✖︎')
+        : colors.green('►►►')
+
+      stats.compilation.name = `🧩 Extension.js ${divider} ${manifestName} (v${manifestVersion})`
+    })
+
+    const projectPath = path.dirname(this.manifestPath)
+    // Support .env files
+    if (
+      fs.existsSync(path.join(projectPath, '.env')) ||
+      fs.existsSync(path.join(projectPath, '.env.example')) ||
+      fs.existsSync(path.join(projectPath, '.env.local')) ||
+      fs.existsSync(path.join(projectPath, '.env.defaults'))
+    ) {
+      console.log(
+        bold(
+          `🧩 Extension.js ${blue(
+            '►►►'
+          )} ${manifestName} (v${manifestVersion}) `
+        ) + `${bold(yellow('env'))} file loaded.`
+      )
+
+      new Dotenv({
+        path: fs.existsSync(path.join(projectPath, '.env'))
+          ? path.join(projectPath, '.env')
+          : fs.existsSync(path.join(projectPath, '.env.local'))
+            ? path.join(projectPath, '.env.local')
+            : path.join(projectPath, '.env.example'),
+        allowEmptyValues: true,
+        defaults: fs.existsSync(path.join(projectPath, '.env.defaults')),
+        systemvars: true
+        // @ts-expect-error - Seems plugin is not up to date
+      }).apply(compiler)
+    }
+  }
+}
