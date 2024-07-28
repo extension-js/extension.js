9880b0a6959d6c855c170bb7621ba48a86f7a281
Cezar Augusto <boss@cezaraugusto.net>
2024-07-28 15:46:57 -0300

    Add __samples__ folder

---
+++ programs/develop/__samples__/templates/content-typescript/background.ts
@@ -0,0 +1,23 @@
+console.log('hello from background script')
+
+// eslint-disable-next-line no-undef
+chrome.runtime.onMessage.addListener((request, sender) => {
+  if (request.action === 'changeBackgroundColor') {
+    changeBackgroundColor(request.color, sender.tab.id)
+  }
+})
+
+function changeBackgroundColor(color, tabId) {
+  // eslint-disable-next-line no-undef
+  chrome.scripting
+    .executeScript({
+      target: {tabId},
+      function: setPageBackgroundColor,
+      args: [color]
+    })
+    .catch(console.error)
+}
+
+function setPageBackgroundColor(color) {
+  document.body.style.backgroundColor = color
+}
