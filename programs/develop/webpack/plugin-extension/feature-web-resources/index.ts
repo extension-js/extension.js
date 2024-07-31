4bc1ad1a9310cf442593f558231c9d3e4661a089
Cezar Augusto <boss@cezaraugusto.net>
2024-07-30 21:22:16 -0300

    Auto add imports from content_scripts to web_accessible_resources

--- programs/develop/webpack/plugin-extension/feature-web-resources/index.ts
+++ programs/develop/webpack/plugin-extension/feature-web-resources/index.ts
@@ -1,7 +1,10 @@
-import type webpack from 'webpack'
-import {type FilepathList, type PluginInterface} from '../../webpack-types'
-// import AddResourcesFromContentScripts from './steps/add-resources-from-content-scripts'
-import {UpdateManifest} from './update-manifest'
+import webpack, {Compilation, Compiler, sources} from 'webpack'
+import {
+  type FilepathList,
+  type PluginInterface,
+  type Manifest
+} from '../../webpack-types'
+import * as utils from '../../lib/utils'
 
 /**
  * ResourcesPlugin is responsible for adding resources required
@@ -23,22 +26,118 @@ export class WebResourcesPlugin {
     this.excludeList = options.excludeList
   }
 
-  apply(compiler: webpack.Compiler) {
-    // new AddResourcesFromContentScripts({
-    //   manifestPath: this.manifestPath,
-    //   includeList: this.includeList,
-    //   excludeList: this.excludeList,
-    // }).apply(compiler)
+  private generateManifestPatches(
+    compilation: webpack.Compilation,
+    entryImports: Record<string, string[]>
+  ) {
+    const manifest = utils.getManifestContent(compilation, this.manifestPath!)
 
-    // 2 - Write the web_accessible_resources folder to the manifest allowlist.
-    // TODO: IncludeList will return a list of script files.
-    // We wait until the ManifestPlugin to generate the reports and get
-    // the assets required by them, and then add it to web_accessible_resources
-    // in the final manifest.json file.
-    new UpdateManifest({
-      manifestPath: this.manifestPath,
-      includeList: this.includeList,
-      excludeList: this.excludeList
-    }).apply(compiler)
+    const webAccessibleResourcesV3: {resources: string[]; matches: string[]}[] =
+      []
+    const webAccessibleResourcesV2: string[] = []
+
+    for (const [entryName, resources] of Object.entries(entryImports)) {
+      const contentScript = manifest.content_scripts?.find((script) => {
+        console.log({
+          script,
+          entryName,
+          includes: script.js?.some((jsFile: string) =>
+            jsFile.includes(entryName)
+          )
+        })
+        return (
+          script.js &&
+          script.js.some((jsFile: string) => jsFile.includes(entryName))
+        )
+      })
+
+      if (contentScript) {
+        const matches = contentScript.matches || []
+        if (manifest.manifest_version === 3) {
+          webAccessibleResourcesV3.push({
+            resources: resources.filter(
+              (resource) => !resource.endsWith('.map')
+            ),
+            matches
+          })
+        } else {
+          webAccessibleResourcesV2.push(...resources)
+        }
+      }
+    }
+
+    if (manifest.manifest_version === 3) {
+      manifest.web_accessible_resources =
+        webAccessibleResourcesV3 as Manifest['web_accessible_resources']
+    } else {
+      // @ts-expect-error web_accessible_resources v2 is not in the manifest v3 type
+      manifest.web_accessible_resources = Array.from(
+        new Set(webAccessibleResourcesV2)
+      )
+    }
+
+    const source = JSON.stringify(manifest, null, 2)
+    const rawSource = new sources.RawSource(source)
+
+    if (compilation.getAsset('manifest.json')) {
+      compilation.updateAsset('manifest.json', rawSource)
+    }
+  }
+
+  apply(compiler: Compiler): void {
+    compiler.hooks.thisCompilation.tap(
+      'plugin-extension:feature-web-resources',
+      (compilation: Compilation) => {
+        compilation.hooks.processAssets.tap(
+          {
+            name: 'plugin-extension:feature-web-resources',
+            stage: Compilation.PROCESS_ASSETS_STAGE_ANALYSE
+          },
+          () => {
+            const contentEntries: string[] = []
+            const entryNames = Object.keys(this.includeList || {})
+
+            for (const key of entryNames.filter(Boolean)) {
+              if (key.startsWith('content_scripts')) {
+                if (Array.isArray(key)) {
+                  contentEntries.push(...key)
+                } else if (typeof key === 'string') {
+                  contentEntries.push(key)
+                }
+              }
+            }
+
+            const chunkGraph = compilation.chunkGraph
+            const entryImports: Record<string, string[]> = {}
+
+            compilation.entrypoints.forEach((entry, entryName) => {
+              if (contentEntries.includes(entryName)) {
+                const importedFiles: string[] = []
+
+                entry.chunks.forEach((chunk) => {
+                  const modules = Array.from(
+                    chunkGraph.getChunkModulesIterable(chunk)
+                  )
+
+                  modules.forEach((module) => {
+                    chunkGraph.getModuleChunks(module).forEach((chunk) => {
+                      chunk.auxiliaryFiles.forEach((file) => {
+                        if (!importedFiles.includes(file)) {
+                          importedFiles.push(file)
+                        }
+                      })
+                    })
+                  })
+                })
+
+                entryImports[entryName] = importedFiles
+              }
+            })
+
+            this.generateManifestPatches(compilation, entryImports)
+          }
+        )
+      }
+    )
   }
 }
