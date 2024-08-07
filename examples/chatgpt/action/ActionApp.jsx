8b519b94b8c803e5887d464fec6215df5acbbd1a
Cezar Augusto <boss@cezaraugusto.net>
2024-08-07 18:20:22 -0300

    ChatGPT example is now an action page

--- examples/chatgpt/action/ActionApp.jsx
+++ examples/chatgpt/action/ActionApp.jsx
@@ -8,7 +8,7 @@ const openai = new OpenAI({
   dangerouslyAllowBrowser: true,
 });
 
-function SidebarApp() {
+function ActionApp() {
   const [messages, setMessages] = useState([
     {
       content: (
@@ -75,7 +75,7 @@ function SidebarApp() {
           <div className="input-group max-w-full w-[800px] relative flex items-center">
             {isTyping && <small className='absolute -top-5 left-0.5 animate-pulse'>ChatGPT Extension is typing...</small>}
 
-            <input type="text" placeholder="Ask ChatGPT a question..." className="input input-bordered flex-grow mr-2.5" required />
+            <input type="text" placeholder="asdasAsk ChatGPT a question!!!." className="input input-bordered flex-grow mr-2.5" required />
             <button className="btn btn-square" type="submit">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="currentColor" viewBox="0 0 16 16">
                 <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
@@ -88,4 +88,4 @@ function SidebarApp() {
   );
 }
 
-export default SidebarApp;
+export default ActionApp;
