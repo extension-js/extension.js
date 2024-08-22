3a6138012dc3eef92550e9431ced354718a9c5b5
Cezar Augusto <boss@cezaraugusto.net>
2024-08-22 16:14:14 -0300

    Fix multiple browsers not running together

--- programs/develop/webpack/plugin-reload/extensions/manager-extension/background.js
+++ programs/develop/webpack/plugin-reload/extensions/manager-extension/background.js
@@ -30,7 +30,7 @@ chrome.tabs.query({active: true}, async ([initialTab]) => {
 ██████████████████████████    ████████████████████████████
 ██████████████████████████████████████████████████████████
 ██████████████████████████████████████████████████████████
-   Make it very easy to develop cross-browser extensions.
+MIT (c) ${new Date().getFullYear()} - Cezar Augusto and the Extension.js Authors.
 `,
     bgGreen('')
   )
