c10b2189d9d7b5a7a4d309dea90d299095320c08
Cezar Augusto <boss@cezaraugusto.net>
2024-09-03 13:09:25 -0300

    Fix Tailwind complains about content key

--- examples/content-shadow-dom/tailwind.config.js
+++ examples/content-shadow-dom/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./content/**/*.html', './content/**/*.js'],
+  content: ['./content/**/*.js'],
   theme: {
     extend: {}
   },
