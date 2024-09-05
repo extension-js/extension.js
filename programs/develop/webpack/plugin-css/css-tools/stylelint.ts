584e1394a534e6330c5bf51498e57027e5af8b2d
Cezar Augusto <boss@cezaraugusto.net>
2024-09-05 15:27:42 -0300

    Delegate Stylelint to the extension.config.js
    stylelint
    - Size: 8.15mb
    
    stylelint-webpack-plugin
    - Size: 7.22mb
    
    stylelint-config-standard-scss
    - Size: 2.5mb
    
    Total: 17.87mb

--- programs/develop/webpack/plugin-css/css-tools/stylelint.ts
+++ programs/develop/webpack/plugin-css/css-tools/stylelint.ts
@@ -9,7 +9,6 @@ import path from 'path'
 import fs from 'fs'
 import {type WebpackPluginInstance} from 'webpack'
 import * as messages from '../../lib/messages'
-import {installOptionalDependencies} from '../../lib/utils'
 
 export function getStylelintConfigFile(projectPath: string) {
   const stylelintConfigJs = path.join(projectPath, 'stylelint.config.json')
@@ -67,40 +66,7 @@ export function isUsingStylelint(projectPath: string) {
 export async function maybeUseStylelint(
   projectPath: string
 ): Promise<WebpackPluginInstance[]> {
-  if (!isUsingStylelint(projectPath)) return []
-
-  try {
-    require.resolve('stylelint')
-  } catch (e) {
-    const stylelintDependencies = [
-      'stylelint',
-      'stylelint-webpack-plugin',
-      'stylelint-config-standard-scss'
-    ]
-    const projectName = require(path.join(projectPath, 'package.json')).name
-    await installOptionalDependencies(
-      projectName,
-      'Stylelint',
-      stylelintDependencies
-    )
-
-    // The compiler will exit after installing the dependencies
-    // as it can't read the new dependencies without a restart.
-
-    console.log(messages.youAreAllSet(projectName, 'Stylelint'))
-    process.exit(0)
-  }
-
-  const StylelintPlugin = require('stylelint-webpack-plugin')
-
-  return [
-    new StylelintPlugin({
-      context: projectPath,
-      configFile: isUsingStylelint(projectPath)
-        ? getStylelintConfigFile(projectPath)
-        : path.join(__dirname, 'stylelint.config.json'),
-      files: '**/*.{css,scss,sass,less}',
-      exclude: ['node_modules', path.join(projectPath, 'node_modules')]
-    })
-  ]
+  isUsingStylelint(projectPath)
+    
+  return []
 }
