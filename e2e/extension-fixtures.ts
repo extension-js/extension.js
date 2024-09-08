aeb6c11797ef2ce4e479a84a1b321390b3a2b852
OSpoon <zxin088@gmail.com>
2024-09-08 12:07:54 +0800

    Disable additional Chrome features to improve test stability

--- e2e/extension-fixtures.ts
+++ e2e/extension-fixtures.ts
@@ -12,6 +12,31 @@ export const extensionFixtures = (pathToExtension: string, headless: boolean) =>
                     headless ? `--headless=new` : '',
                     `--disable-extensions-except=${pathToExtension}`,
                     `--load-extension=${pathToExtension}`,
+                    '--no-first-run',                                                   // Disable Chrome's native first run experience.
+                    '--disable-client-side-phishing-detection',                         // Disables client-side phishing detection
+                    '--disable-component-extensions-with-background-pages',             // Disable some built-in extensions that aren't affected by '--disable-extensions'
+                    '--disable-default-apps',                                           // Disable installation of default apps
+                    '--disable-features=InterestFeedContentSuggestions',                // Disables the Discover feed on NTP
+                    '--disable-features=Translate',                                     // Disables Chrome translation, both the manual option and the popup prompt when a page with differing language is detected.
+                    '--hide-scrollbars',                                                // Hide scrollbars from screenshots.
+                    '--mute-audio',                                                     // Mute any audio
+                    '--no-default-browser-check',                                       // Disable the default browser check, do not prompt to set it as such
+                    '--no-first-run',                                                   // Skip first run wizards
+                    '--ash-no-nudges',                                                  // Avoids blue bubble "user education" nudges (eg., "… give your browser a new look", Memory Saver)
+                    '--disable-search-engine-choice-screen',                            // Disable the 2023+ search engine choice screen
+                    '--disable-features=MediaRoute',                                    // Avoid the startup dialog for `Do you want the application “Chromium.app” to accept incoming network connections?`.  Also disables the Chrome Media Router which creates background networking activity to discover cast targets. A superset of disabling DialMediaRouteProvider.
+                    '--use-mock-keychain',                                              // Use mock keychain on Mac to prevent the blocking permissions dialog about "Chrome wants to use your confidential information stored in your keychain"
+                    '--disable-background-networking',                                  // Disable various background network services, including extension updating, safe browsing service, upgrade detector, translate, UMA
+                    '--disable-breakpad',                                               // Disable crashdump collection (reporting is already disabled in Chromium)
+                    '--disable-component-update',                                       // Don't update the browser 'components' listed at chrome://components/
+                    '--disable-domain-reliability',                                     // Disables Domain Reliability Monitoring, which tracks whether the browser has difficulty contacting Google-owned sites and uploads reports to Google.
+                    '--disable-features=AutofillServerCommunicatio',                    // Disables autofill server communication. This feature isn't disabled via other 'parent' flags.
+                    '--disable-features=CertificateTransparencyComponentUpdate',
+                    '--disable-sync',                                                   // Disable syncing to a Google account
+                    '--disable-features=OptimizationHints',                             // Used for turning on Breakpad crash reporting in a debug environment where crash reporting is typically compiled but disabled. Disable the Chrome Optimization Guide and networking with its service API
+                    '--disable-features=DialMediaRouteProvider',                        // A weaker form of disabling the MediaRouter feature. See that flag's details.
+                    '--no-pings',                                                       // Don't send hyperlink auditing pings
+                    '--enable-features=SidePanelUpdates',                               // Ensure the side panel is visible. This is used for testing the side panel feature.
                 ].filter(arg => !!arg),
             });
             await use(context);
