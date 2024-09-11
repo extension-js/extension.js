1fc615e6e2b1a459d6f298dc7c58ba2f006da7b6
Cezar Augusto <boss@cezaraugusto.net>
2024-09-11 18:36:46 -0300

    Add content-svgr template

---
+++ examples/content-react-svgr/extension.config.js
@@ -0,0 +1,22 @@
+/** @type {import('extension-develop').FileConfig} */
+module.exports = {
+  config: (config) => {
+    config.module.rules.push(
+      {
+        test: /\.svg$/i,
+        type: 'asset',
+        // *.svg?url
+        resourceQuery: /url/
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        // exclude react component if *.svg?url
+        resourceQuery: {not: [/url/]},
+        use: ['@svgr/webpack']
+      }
+    )
+
+    return config
+  }
+}
