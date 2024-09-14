85d2bc0df06d0e7a601c2771e4b523e412f98212
George Karagkiaouris <gkaragkiaouris2@gmail.com>
2024-09-13 20:57:28 -0400

    Fix web_resources match patterns (#178)
    * Fix web_resources match patterns

---
+++ programs/develop/webpack/plugin-extension/feature-web-resources/__spec__/clean-matches.spec.ts
@@ -0,0 +1,34 @@
+
+import { cleanMatches } from "../clean-matches";
+
+describe('cleanMatches', () => {
+  it('does not handle non-urls', () => {
+    expect(cleanMatches(['<all_urls>'])).toEqual(['<all_urls>']);
+  });
+
+  it('handles wildcards', () => {
+    expect(
+      cleanMatches([
+        '*://*/some/path',
+        'https://*.google.com/foo*bar',
+      ])
+    ).toEqual([
+      '*://*/*',
+      'https://*.google.com/*',
+    ]);
+  });
+
+  it('cleans up all types of pathnames', () => {
+    expect(
+      cleanMatches([
+        'https://example.com',
+        'https://example.com/some/path/*',
+        'https://example.com/some/path',
+      ])
+    ).toEqual([
+      'https://example.com/*',
+      'https://example.com/*',
+      'https://example.com/*',
+    ]);
+  });
+});
