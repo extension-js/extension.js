5a1eef0e740c6880aa840ea2a120d9cb6bd3548f
Cezar Augusto <boss@cezaraugusto.net>
2024-09-13 22:09:30 -0300

    v2.0.0-alpha.28

--- programs/develop/webpack/plugin-extension/feature-web-resources/__spec__/clean-matches.spec.ts
+++ programs/develop/webpack/plugin-extension/feature-web-resources/__spec__/clean-matches.spec.ts
@@ -1,34 +1,27 @@
-
-import { cleanMatches } from "../clean-matches";
+import {cleanMatches} from '../clean-matches'
 
 describe('cleanMatches', () => {
   it('does not handle non-urls', () => {
-    expect(cleanMatches(['<all_urls>'])).toEqual(['<all_urls>']);
-  });
+    expect(cleanMatches(['<all_urls>'])).toEqual(['<all_urls>'])
+  })
 
   it('handles wildcards', () => {
     expect(
-      cleanMatches([
-        '*://*/some/path',
-        'https://*.google.com/foo*bar',
-      ])
-    ).toEqual([
-      '*://*/*',
-      'https://*.google.com/*',
-    ]);
-  });
+      cleanMatches(['*://*/some/path', 'https://*.google.com/foo*bar'])
+    ).toEqual(['*://*/*', 'https://*.google.com/*'])
+  })
 
   it('cleans up all types of pathnames', () => {
     expect(
       cleanMatches([
         'https://example.com',
         'https://example.com/some/path/*',
-        'https://example.com/some/path',
+        'https://example.com/some/path'
       ])
     ).toEqual([
       'https://example.com/*',
       'https://example.com/*',
-      'https://example.com/*',
-    ]);
-  });
-});
+      'https://example.com/*'
+    ])
+  })
+})
