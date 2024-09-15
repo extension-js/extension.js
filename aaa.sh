dd41e7fd56afb01a9ca8207d73b6969cc09e4a12
Cezar Augusto <boss@cezaraugusto.net>
2024-09-15 15:20:47 -0300

    Add missing templates to tests

--- aaa.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Set the base directory
-BASE_DIR="./examples"
-
-# Find and delete all "dist" directories inside the ./examples/* directories
-find "$BASE_DIR" -type d -name "dist" -exec rm -rf {} +
-
-echo "All 'dist' directories inside $BASE_DIR have been deleted."
