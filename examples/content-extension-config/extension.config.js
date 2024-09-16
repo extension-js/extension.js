a11c98ea5610386dbb79e52edad89a8b0e42dc44
Cezar Augusto <boss@cezaraugusto.net>
2024-09-16 17:17:06 -0300

    Update template's extension.config.js

--- examples/content-extension-config/extension.config.js
+++ examples/content-extension-config/extension.config.js
@@ -1,29 +1,36 @@
-/** @type {import('extension-develop').FileConfig} */
+/** @type {import('extension').FileConfig} */
 module.exports = {
   browsers: {
     chrome: {
+      // noOpen?: boolean
+      // userDataDir?: string
+      // profile?: string
+      // preferences?: Record<string, any>
+      // browserFlags?: string[]
+      // startingUrl?: string
+      // chromiumBinary?: string
       startingUrl: 'https://extension.js.org'
     },
     firefox: {
+      // noOpen?: boolean
+      // userDataDir?: string
+      // profile?: string
+      // preferences?: Record<string, any>
+      // browserFlags?: string[]
+      // startingUrl?: string
+      // geckoBinary?: string
       startingUrl: 'about:debugging#/runtime/this-firefox'
     }
   },
-  commands: {
-    dev: {
-      browser: 'firefox',
-      polyfill: true
-    },
-    start: {
-      browser: 'firefox',
-      polyfill: true
-    },
-    preview: {
-      browser: 'firefox',
-      polyfill: true
-    },
-    build: {
-      browser: 'firefox',
-      polyfill: true
-    }
-  }
+  development: {
+    // TBD
+    // browser: DevOptions['browser']
+    // port?: number
+    // browser: 'firefox', // can be an array
+    // zipFilename?: string
+    // zip?: boolean
+    // zipSource?: boolean
+    // polyfill?: boolean
+  },
+  deployment: {}
 }
