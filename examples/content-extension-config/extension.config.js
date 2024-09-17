afbc13976ad93096ac6a10d24aa4ffaea4416b83
Cezar Augusto <boss@cezaraugusto.net>
2024-09-17 00:24:30 -0300

    Extended extension.config.js (#188)

--- examples/content-extension-config/extension.config.js
+++ examples/content-extension-config/extension.config.js
@@ -1,22 +1,36 @@
-/** @type {import('extension-develop').FileConfig} */
+/** @type {import('extension').FileConfig} */
 module.exports = {
-  config: (config) => {
-    config.module.rules.push(
-      {
-        test: /\.svg$/i,
-        type: 'asset',
-        // *.svg?url
-        resourceQuery: /url/
-      },
-      {
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        // exclude react component if *.svg?url
-        resourceQuery: {not: [/url/]},
-        use: ['@svgr/webpack']
-      }
-    )
-
-    return config
-  }
+  browsers: {
+    chrome: {
+      // noOpen?: boolean
+      // userDataDir?: string
+      // profile?: string
+      // preferences?: Record<string, any>
+      // browserFlags?: string[]
+      // startingUrl?: string
+      // chromiumBinary?: string
+      startingUrl: 'https://extension.js.org'
+    },
+    firefox: {
+      // noOpen?: boolean
+      // userDataDir?: string
+      // profile?: string
+      // preferences?: Record<string, any>
+      // browserFlags?: string[]
+      // startingUrl?: string
+      // geckoBinary?: string
+      startingUrl: 'about:debugging#/runtime/this-firefox'
+    }
+  },
+  development: {
+    // TBD
+    // browser: DevOptions['browser']
+    // port?: number
+    // browser: 'firefox', // can be an array
+    // zipFilename?: string
+    // zip?: boolean
+    // zipSource?: boolean
+    // polyfill?: boolean
+  },
+  deployment: {}
 }
