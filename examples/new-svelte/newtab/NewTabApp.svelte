09e7785b4daed5281052d9b3841db4660c6fd1fd
0xakash9 <devakash256@gmail.com>
2024-09-25 22:52:51 +0530

    feat: add svelte support

---
+++ examples/new-svelte/newtab/NewTabApp.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  let message = 'Welcome to your Svelte Extension.'
+</script>
+
+<header>
+  <h1>
+    <img class="svelte" src="/logo.png" alt="The Svelte logo" width="120px" />
+    <br />
+    {message}.
+  </h1>
+  <p>
+    Learn more about creating browser extensions at
+    <a href="https://extension.js.org" target="_blank">
+      https://extension.js.org
+    </a>
+    .
+  </p>
+</header>
---
+++ programs/develop/webpack/plugin-js-frameworks/js-tools/svelte.ts
@@ -0,0 +1,82 @@
+// ██████╗ ███████╗██╗   ██╗███████╗██╗      ██████╗ ██████╗
+// ██╔══██╗██╔════╝██║   ██║██╔════╝██║     ██╔═══██╗██╔══██╗
+// ██║  ██║█████╗  ██║   ██║█████╗  ██║     ██║   ██║██████╔╝
+// ██║  ██║██╔══╝  ╚██╗ ██╔╝██╔══╝  ██║     ██║   ██║██╔═══╝
+// ██████╔╝███████╗ ╚████╔╝ ███████╗███████╗╚██████╔╝██║
+// ╚═════╝ ╚══════╝  ╚═══╝  ╚══════╝╚══════╝ ╚═════╝ ╚═╝
+
+import path from 'path'
+import fs from 'fs'
+import * as messages from '../../lib/messages'
+import {installOptionalDependencies} from '../../lib/utils'
+import {JsFramework} from '../../webpack-types'
+
+let userMessageDelivered = false
+
+export function isUsingSvelte(projectPath: string) {
+  const packageJsonPath = path.join(projectPath, 'package.json')
+
+  if (!fs.existsSync(packageJsonPath)) {
+    return false
+  }
+
+  const packageJson = require(packageJsonPath)
+
+  const svelteAsDevDep =
+    packageJson.devDependencies && packageJson.devDependencies?.svelte
+  const svelteAsDep =
+    packageJson.dependencies && packageJson.dependencies.svelte
+
+  if (true || svelteAsDep) {
+    if (!userMessageDelivered) {
+      if (process.env.EXTENSION_ENV === 'development') {
+        console.log(messages.isUsingIntegration('Svelte'))
+      }
+
+      userMessageDelivered = true
+    }
+  }
+
+  return !!svelteAsDevDep || !!svelteAsDep
+}
+
+export async function maybeUseSvelte(
+  projectPath: string
+): Promise<JsFramework | undefined> {
+  if (!isUsingSvelte(projectPath)) return undefined
+
+  try {
+    require.resolve('svelte-loader')
+  } catch (e) {
+    const typeScriptDependencies = ['typescript']
+
+    await installOptionalDependencies('TypeScript', typeScriptDependencies)
+
+    const svelteDependencies = ['svelte-loader', 'svelte-preprocess']
+
+    await installOptionalDependencies('Svelte', svelteDependencies)
+
+    // The compiler will exit after installing the dependencies
+    // as it can't read the new dependencies without a restart.
+    console.log(messages.youAreAllSet('Svelte'))
+    process.exit(0)
+  }
+
+  const svelteLoaders: JsFramework['loaders'] = [
+    {
+      test: /\.svelte$/,
+      loader: require.resolve('svelte-loader'),
+      include: projectPath,
+      exclude: /node_modules/,
+      options: {
+        preprocess: require('svelte-preprocess')()
+      }
+    }
+  ]
+
+  return {
+    plugins: undefined,
+    loaders: svelteLoaders,
+    alias: undefined
+  }
+}
