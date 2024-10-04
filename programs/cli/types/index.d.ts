173dcb4c600c9a52f340196e8363713e01148cbd
Cezar Augusto <boss@cezaraugusto.net>
2024-10-04 10:24:36 -0300

    Create js-frameworks.d.ts, ignore examples/* for tsconfig

--- programs/cli/types/index.d.ts
+++ programs/cli/types/index.d.ts
@@ -1,11 +1,9 @@
 /// <reference types="node" />
-/// <reference types="react" />
-/// <reference types="react-dom" />
 /// <reference types="chrome" />
+/// <reference types="./js-frameworks.d.ts" />
 /// <reference path="./css-content.d.ts" />
 /// <reference path="./css-modules.d.ts" />
 /// <reference path="./images.d.ts" />
-/// <reference types="svelte" />
 
 declare namespace NodeJS {
   interface ProcessEnv {
---
+++ programs/cli/types/js-frameworks.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="react" />
+/// <reference types="react-dom" />
+/// <reference types="svelte" />
