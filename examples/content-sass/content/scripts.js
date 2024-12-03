d6f313a7e0e899e834e4814eefaa63814733b953
Cezar Augusto <boss@cezaraugusto.net>
2024-12-03 20:01:48 -0300

    Revert "Revert "Update all content-* templates""
    This reverts commit 851a519c1584597e40015a6d8f7a56f61cd60f75.

--- examples/content-sass/content/scripts.js
+++ examples/content-sass/content/scripts.js
@@ -3,21 +3,38 @@ import logo from '../images/logo.svg'
 
 console.log('hello from content_scripts')
 
-// Check if the content has already been added
-document.body.innerHTML += `
-<div class="content_script">
-  <img class="content_logo" src="${logo}" />
-  <h1 class="content_title">
-    Welcome to your Sass Extension
-  </h1>
-  <p class="content_description">
-    Learn more about creating cross-browser extensions at <a
-      className="underline hover:no-underline"
-      href="https://extension.js.org"
-      target="_blank"
-    >
-    https://extension.js.org
-    </a>
-  </p>
-</div>
+setTimeout(initial, 1000)
+
+function initial() {
+  const rootDiv = document.createElement('div')
+  rootDiv.id = 'extension-root'
+  document.body.appendChild(rootDiv)
+
+  // Injecting content_scripts inside a shadow dom
+  // prevents conflicts with the host page's styles.
+  // This way, styles from the extension won't leak into the host page.
+  const shadowRoot = rootDiv.attachShadow({mode: 'open'})
+
+  if (process.env.EXTENSION_MODE === 'development') {
+    // Use the shadow root as the root element to inject styles into.
+    window.__EXTENSION_SHADOW_ROOT__ = shadowRoot
+  }
+
+  shadowRoot.innerHTML = `
+    <div class="content_script">
+      <img class="content_logo" src="${logo}" />
+      <h1 class="content_title">
+        Welcome to your Sass Extension
+      </h1>
+      <p class="content_description">
+        Learn more about creating cross-browser extensions at <a
+          className="underline hover:no-underline"
+          href="https://extension.js.org"
+          target="_blank"
+        >
+        https://extension.js.org
+        </a>
+      </p>
+    </div>
   `
+}
--- examples/content-typescript/content/scripts.ts
+++ examples/content-typescript/content/scripts.ts
@@ -3,20 +3,39 @@ import logo from '../images/logo.svg'
 
 console.log('hello from content_scripts')
 
-document.body.innerHTML += `
-<div class="content_script">
-  <img class="content_logo" src="${logo}" />
-  <h1 class="content_title">
-    Welcome to your TypeScript Extension
-  </h1>
-  <p class="content_description">
-    Learn more about creating cross-browser extensions at <a
-      className="underline hover:no-underline"
-      href="https://extension.js.org"
-      target="_blank"
-    >
-    https://extension.js.org
-    </a>
-  </p>
-</div>
-`
+setTimeout(initial, 1000)
+
+function initial() {
+  const rootDiv = document.createElement('div')
+  rootDiv.id = 'extension-root'
+
+  // Injecting content_scripts inside a shadow dom
+  // prevents conflicts with the host page's styles.
+  // This way, styles from the extension won't leak into the host page.
+  const shadowRoot = rootDiv.attachShadow({mode: 'open'})
+
+  // Tell Extension.js to use the shadow root as the root element
+  // to inject styles into.
+  // @ts-exspect-error - Ignore TS error for global variable
+  window.__EXTENSION_SHADOW_ROOT__ = shadowRoot
+
+  document.body.appendChild(rootDiv)
+
+  shadowRoot.innerHTML = `
+    <div class="content_script">
+      <img class="content_logo" src="${logo}" />
+      <h1 class="content_title">
+        Welcome to your TypeScript Extension
+      </h1>
+      <p class="content_description">
+        Learn more about creating cross-browser extensions at <a
+          className="underline hover:no-underline"
+          href="https://extension.js.org"
+          target="_blank"
+        >
+        https://extension.js.org
+        </a>
+      </p>
+    </div>
+  `
+}
