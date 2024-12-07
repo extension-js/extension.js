5981c6d6dfad225af1fcafe5ef6844f7ca6a5ff8
Cezar Augusto <boss@cezaraugusto.net>
2024-12-07 10:47:36 -0300

    Update all flaky tests

--- examples/new-typescript/template.spec.ts
+++ examples/new-typescript/template.spec.ts
@@ -16,8 +16,12 @@ test('should exist an element with the welcome message text', async ({
   page
 }) => {
   await page.goto('chrome://newtab/')
-  const h1 = page.locator('h1')
-  test.expect(h1).toContainText('Welcome to your')
+  const h1 = await page.waitForSelector('h1', {
+    state: 'visible',
+    timeout: 10000
+  })
+  const textContent = await h1.textContent()
+  test.expect(textContent).toMatch(/Welcome to your/i)
 })
 
 test('should exist a default color value', async ({page}) => {
