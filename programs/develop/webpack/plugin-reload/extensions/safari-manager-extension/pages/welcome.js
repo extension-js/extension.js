53c239fa4a8da29678a5c4a4103dbc65e1ca80a8
Cezar Augusto <boss@cezaraugusto.net>
2024-12-12 12:47:34 -0300

    Add Safari manager extension

---
+++ programs/develop/webpack/plugin-reload/extensions/safari-manager-extension/pages/welcome.js
@@ -0,0 +1,34 @@
+async function getUserExtension() {
+  const allExtensions = await chrome.management.getAll()
+
+  return allExtensions.filter((extension) => {
+    return (
+      // Do not include itself
+      extension.id !== chrome.runtime.id &&
+      // Reload extension
+      extension.id !== 'igcijhgmihmjbbahdabahfbpffalcfnn' &&
+      // Show only unpackaged extensions
+      extension.installType === 'development'
+    )
+  })
+}
+
+async function onStartup() {
+  const userExtension = await getUserExtension()
+  const extensionName = document.getElementById('extensionName')
+  const extensionDescription = document.getElementById('extensionDescription')
+
+  extensionName.innerText = userExtension[0].name
+  extensionName.title = `• Name: ${userExtension[0].name}
+• ID: ${userExtension[0].id}
+• Version: ${userExtension[0].version}`
+
+  extensionDescription.innerText = userExtension[0].description
+
+  const learnMore = document.getElementById('learnMore')
+  learnMore.addEventListener('click', () => {
+    chrome.tabs.create({url: 'https://extension.js.org/'})
+  })
+}
+
+onStartup()
