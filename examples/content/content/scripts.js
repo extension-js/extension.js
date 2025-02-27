5ebacdfe088646bd15e786b78d015d50fe07481b
Cezar Augusto <boss@cezaraugusto.net>
2025-02-27 08:57:01 -0300

    Update content example

--- examples/content/content/scripts.js
+++ examples/content/content/scripts.js
@@ -3,11 +3,15 @@ import logo from '../images/logo.svg'
 
 console.log('hello from content_scripts')
 
+let unmount
+import.meta.webpackHot.accept()
+import.meta.webpackHot.dispose(() => unmount())
+
 if (document.readyState === 'complete') {
-  initial()
+  unmount = initial() || (() => {})
 } else {
   document.addEventListener('readystatechange', () => {
-    if (document.readyState === 'complete') initial()
+    if (document.readyState === 'complete') unmount = initial() || (() => {})
   })
 }
 
@@ -22,23 +26,61 @@ function initial() {
   const shadowRoot = rootDiv.attachShadow({mode: 'open'})
 
   // Inform Extension.js that the shadow root is available.
-  window.__EXTENSION_SHADOW_ROOT__ = shadowRoot
+  // window.__EXTENSION_SHADOW_ROOT__ = shadowRoot
 
-  shadowRoot.innerHTML = `
-    <div class="content_script">
-      <img class="content_logo" src="${logo}" />
-      <h1 class="content_title">
-        Welcome to your Content Script Extension
-      </h1>
-      <p class="content_description">
-        Learn more about creating cross-browser extensions at <a
-          className="underline hover:no-underline"
-          href="https://extension.js.org"
-          target="_blank"
-        >
-        https://extension.js.org
-        </a>
-      </p>
-    </div>
-  `
+  const style = document.createElement('style')
+  shadowRoot.appendChild(style)
+
+  const contentDiv = document.createElement('div')
+  contentDiv.className = 'content_script'
+
+  const logoImg = document.createElement('img')
+  logoImg.className = 'content_logo'
+  logoImg.src = logo
+  contentDiv.appendChild(logoImg)
+
+  const title = document.createElement('h1')
+  title.className = 'content_title'
+  title.textContent = 'Welcome to your Content Script Extension'
+  contentDiv.appendChild(title)
+
+  const description = document.createElement('p')
+  description.className = 'content_description'
+
+  const text = document.createTextNode(
+    'Learn more about creating cross-browser extensions at '
+  )
+  description.appendChild(text)
+
+  const link = document.createElement('a')
+  link.className = 'underline hover:no-underline'
+  link.href = 'https://extension.js.org'
+  link.target = '_blank'
+  link.textContent = 'https://extension.js.org'
+
+  description.appendChild(link)
+  contentDiv.appendChild(description)
+
+  shadowRoot.appendChild(contentDiv)
+
+  // Handle CSS injection
+  fetchCSS().then((response) => {
+    style.textContent = response
+  })
+
+  import.meta.webpackHot?.accept('./styles.css', () => {
+    fetchCSS().then((response) => {
+      style.textContent = response
+    })
+  })
+
+  return () => {
+    rootDiv.remove()
+  }
+}
+
+async function fetchCSS() {
+  const response = await fetch(new URL('./styles.css', import.meta.url))
+  const text = await response.text()
+  return response.ok ? text : Promise.reject(text)
 }
