a62cf2d5f91f80c21b1f0039fdd2d711659c1fad
Cezar Augusto <boss@cezaraugusto.net>
2025-03-19 14:50:31 -0300

    Do not clean output during `preview`

--- programs/develop/webpack/webpack-config.ts
+++ programs/develop/webpack/webpack-config.ts
@@ -26,6 +26,10 @@ export default function webpackConfig(
   devOptions: DevOptions & {
     preferences?: Record<string, string>
     browserFlags?: string[]
+  } & {
+    output: {
+      clean: boolean
+    }
   }
 ): Configuration {
   const manifestPath = path.join(projectPath, 'manifest.json')
@@ -54,7 +58,7 @@ export default function webpackConfig(
         ? 'cheap-source-map'
         : 'eval-cheap-source-map',
     output: {
-      clean: false,
+      clean: devOptions.output?.clean,
       path: userExtensionOutputPath,
       // See https://webpack.js.org/configuration/output/#outputpublicpath
       publicPath: '/',
