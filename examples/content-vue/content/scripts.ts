3b753292c0f320dc777fe45e02d7a4e17f696d68
Cezar Augusto <boss@cezaraugusto.net>
2025-03-25 20:43:43 -0300

    Update content-vue template

--- examples/content-vue/content/scripts.ts
+++ examples/content-vue/content/scripts.ts
@@ -1,34 +1,61 @@
 import {createApp} from 'vue'
 import ContentApp from './ContentApp.vue'
-import './styles.css?inline_style'
+
+let unmount: () => void
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot?.accept()
+  import.meta.webpackHot?.dispose(() => unmount?.())
+}
+
+if (document.readyState === 'complete') {
+  unmount = initial() || (() => {})
+} else {
+  document.addEventListener('readystatechange', () => {
+    if (document.readyState === 'complete') unmount = initial() || (() => {})
+  })
+}
+
+console.log('Hello from content script')
 
 function initial() {
-  // Create a new div element and append it to the document's body
   const rootDiv = document.createElement('div')
   rootDiv.id = 'extension-root'
   document.body.appendChild(rootDiv)
 
-  // Inject content_scripts inside a shadow DOM
-  // to prevent conflicts with the host page's styles.
   const shadowRoot = rootDiv.attachShadow({mode: 'open'})
 
-  window.__EXTENSION_SHADOW_ROOT__ = shadowRoot
+  const style = document.createElement('style')
+  shadowRoot.appendChild(style)
 
-  // Create a container inside the shadow DOM for the Vue app
-  const shadowAppContainer = document.createElement('div')
-  shadowAppContainer.className = 'content_script'
-  shadowRoot.appendChild(shadowAppContainer)
+  fetchCSS().then((response) => {
+    style.textContent = response
+  })
+
+  if (import.meta.webpackHot) {
+    import.meta.webpackHot?.accept('./styles.css', () => {
+      fetchCSS().then((response) => {
+        style.textContent = response
+      })
+    })
+  }
+
+  // Create container for Svelte app
+  const contentDiv = document.createElement('div')
+  contentDiv.className = 'content_script'
+  shadowRoot.appendChild(contentDiv)
 
   // Mount the Vue app to the container inside the shadow DOM
   const app = createApp(ContentApp)
-  app.mount(shadowAppContainer)
+  app.mount(contentDiv)
+
+  return () => {
+    rootDiv.remove()
+  }
 }
 
-// Initialize the app
-if (document.readyState === 'complete') {
-  initial()
-} else {
-  document.addEventListener('readystatechange', () => {
-    if (document.readyState === 'complete') initial()
-  })
+async function fetchCSS() {
+  const response = await fetch(new URL('./styles.css', import.meta.url))
+  const text = await response.text()
+  return response.ok ? text : Promise.reject(text)
 }
