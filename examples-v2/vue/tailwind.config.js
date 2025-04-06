aa7649a2e7a6f0a4ce6804aff5e54be9799fd5c6
Cezar Augusto <boss@cezaraugusto.net>
2025-04-06 03:24:29 -0300

    Update all examples

--- examples-v2/vue/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['**/*.vue'],
-  theme: {
-    extend: {}
-  },
-  plugins: []
-}
--- examples/action-chatgpt/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./action/**/*.tsx'],
-  theme: {
-    extend: {}
-  },
-  plugins: [require('daisyui')]
-}
