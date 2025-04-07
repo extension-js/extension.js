d20ca202d2b6e813a4d752cbb7943adb26cb48e0
Cezar Augusto <boss@cezaraugusto.net>
2025-04-07 18:32:51 -0300

    Fix `process` breaking cypto template

--- examples/new-crypto/extension.config.js
+++ examples/new-crypto/extension.config.js
@@ -3,12 +3,7 @@ const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
 /** @type {import('extension').FileConfig} */
 module.exports = {
   config: (config) => {
-    config.plugins = [
-      ...config.plugins,
-      new NodePolyfillPlugin({
-        additionalAliases: ['process']
-      })
-    ]
+    config.plugins = [...config.plugins, new NodePolyfillPlugin()]
 
     return config
   }
