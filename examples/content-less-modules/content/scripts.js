3df1ba00087cf6f3c45315831726a7bcf91ca410
Cezar Augusto <boss@cezaraugusto.net>
2025-04-08 20:25:28 -0300

    Re-enable partial SASS/LESS modules support for content_scripts

--- examples/content-less-modules/content/scripts.js
+++ examples/content-less-modules/content/scripts.js
@@ -1,14 +1,25 @@
-import './styles.less?inline_style'
-import styles from './Logo.module.less?inline_style'
 import logo from '../images/logo.svg'
+import {
+  content_script,
+  content_logo,
+  content_title,
+  content_description
+} from './styles.module.less'
+
+let unmount
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot?.accept()
+  import.meta.webpackHot?.dispose(() => unmount?.())
+}
 
 console.log('hello from content_scripts')
 
 if (document.readyState === 'complete') {
-  initial()
+  unmount = initial() || (() => {})
 } else {
   document.addEventListener('readystatechange', () => {
-    if (document.readyState === 'complete') initial()
+    if (document.readyState === 'complete') unmount = initial() || (() => {})
   })
 }
 
@@ -21,25 +32,54 @@ function initial() {
   // prevents conflicts with the host page's styles.
   // This way, styles from the extension won't leak into the host page.
   const shadowRoot = rootDiv.attachShadow({mode: 'open'})
+  const style = new CSSStyleSheet()
+  shadowRoot.adoptedStyleSheets = [style]
 
-  // Inform Extension.js that the shadow root is available.
-  window.__EXTENSION_SHADOW_ROOT__ = shadowRoot
+  // Fetch and apply LESS styles
+  fetchLessStyles().then((response) => style.replace(response))
 
-  shadowRoot.innerHTML = `
-  <div class="content_script">
-    <img class="${styles.logo}" src="${logo}" />
-    <h1 class="content_title">
-      Welcome to your LESS Modules Extension
-    </h1>
-    <p class="content_description">
-      Learn more about creating cross-browser extensions at <a
-        className="underline hover:no-underline"
-        href="https://extension.js.org"
-        target="_blank"
-      >
-      https://extension.js.org
-      </a>
-    </p>
-  </div>
-  `
+  // Create container div
+  const contentDiv = document.createElement('div')
+  contentDiv.className = content_script
+
+  // Create and append logo image
+  const img = document.createElement('img')
+  img.className = content_logo
+  img.src = logo
+  contentDiv.appendChild(img)
+
+  // Create and append title
+  const title = document.createElement('h1')
+  title.className = content_title
+  title.textContent = 'Welcome to your LESS Modules Extension'
+  contentDiv.appendChild(title)
+
+  // Create and append description paragraph
+  const desc = document.createElement('p')
+  desc.className = content_description
+  desc.innerHTML = 'Learn more about creating cross-browser extensions at '
+
+  const link = document.createElement('a')
+  link.href = 'https://extension.js.org'
+  link.target = '_blank'
+  link.textContent = 'https://extension.js.org'
+
+  desc.appendChild(link)
+  contentDiv.appendChild(desc)
+
+  // Append the content div to shadow root
+  shadowRoot.appendChild(contentDiv)
+
+  return () => {
+    rootDiv.remove()
+  }
+}
+
+// IMPORTANT: Hot reloading of LESS modules is not supported.
+// You need to reload the current tab to see the changes.
+async function fetchLessStyles() {
+  const lessUrl = chrome.runtime.getURL('/content_scripts/content-0.css')
+  const response = await fetch(lessUrl)
+  const text = await response.text()
+  return response.ok ? text : Promise.reject(text)
 }
