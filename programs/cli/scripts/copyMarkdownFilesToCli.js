4246b8f47ae4a9048d60cdd67ba6804bd5dc75c6
Cezar Augusto <boss@cezaraugusto.net>
2025-05-05 15:34:52 -0300

    All core modules as ESM

--- programs/cli/scripts/copyMarkdownFilesToCli.js
+++ programs/cli/scripts/copyMarkdownFilesToCli.js
@@ -5,8 +5,12 @@
 // ╚██████╗███████╗██║
 //  ╚═════╝╚══════╝╚═╝
 
-const fs = require('fs/promises')
-const path = require('path')
+import fs from 'fs/promises'
+import path from 'path'
+import {fileURLToPath} from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const sourceDir = path.resolve(__dirname, '../../../')
 const targetDir = path.resolve(__dirname, '../')
