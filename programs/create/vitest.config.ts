428454cbefcc27d4b4f2d1b98a08cd60f278d633
Cezar Augusto <boss@cezaraugusto.net>
2025-05-07 20:33:34 -0300

    Update test config

--- programs/create/vitest.config.ts
+++ programs/create/vitest.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from 'vitest/config'
 
 export default defineConfig({
   test: {
+    pool: 'forks',
     globals: true,
     environment: 'node',
     include: ['**/*.spec.ts'],
