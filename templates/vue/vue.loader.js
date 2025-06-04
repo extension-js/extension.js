9b1a2e7b8321f8e207414d30691975d7f7802a07
Cezar Augusto <boss@cezaraugusto.net>
2025-06-04 13:34:40 -0300

    Add support to Vue/Svelte loader files

---
+++ templates/vue/vue.loader.js
@@ -0,0 +1,11 @@
+// This is an optional file for Vue projects by Extension.js.
+// It allows you to adapt vue-loader's behavior to your project needs.
+// If you don't need it, you can remove it.
+// Original implementation: programs/develop/webpack/plugin-js-frameworks/js-tools/vue.ts
+
+export default (options) => {
+  return {
+    // https://vue-loader.vuejs.org/options.html#options-reference
+    ...options
+  }
+}
