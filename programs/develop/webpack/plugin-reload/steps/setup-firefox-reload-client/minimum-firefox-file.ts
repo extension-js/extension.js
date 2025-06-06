3110598ba00675105c972a9457f4455ccb75ce56
Cezar Augusto <boss@cezaraugusto.net>
2025-06-05 21:35:53 -0300

    Fix vulnerabilities found by npm audit (#283)
    * Fix vulnerabilities found by npm audit

--- programs/develop/webpack/plugin-reload/steps/setup-firefox-reload-client/minimum-firefox-file.ts
+++ programs/develop/webpack/plugin-reload/steps/setup-firefox-reload-client/minimum-firefox-file.ts
@@ -1,28 +1,30 @@
-browser.runtime.onMessageExternal.addListener(async (request: any, _sender: any) => {
-  const managementInfo = await browser.management.getSelf()
+browser.runtime.onMessageExternal.addListener(
+  async (request: any, _sender: any) => {
+    const managementInfo = await browser.management.getSelf()
 
-  // Ping-pong between the user extension background page(this)
-  // and the middleware socket client (reloadService.ts),
-  // which will then send a message to the server
-  // (startServer.ts) so it can display the extension info.
-  if (request.initialLoadData) {
-    return {
-      id: browser.runtime.id,
-      manifest: browser.runtime.getManifest(),
-      management: managementInfo
+    // Ping-pong between the user extension background page(this)
+    // and the middleware socket client (reloadService.ts),
+    // which will then send a message to the server
+    // (startServer.ts) so it can display the extension info.
+    if (request.initialLoadData) {
+      return {
+        id: browser.runtime.id,
+        manifest: browser.runtime.getManifest(),
+        management: managementInfo
+      }
     }
-  }
 
-  // Reload the extension runtime if the manifest or
-  // service worker changes.
-  if (
-    request.changedFile === 'declarative_net_request' ||
-    request.changedFile === 'manifest.json' ||
-    request.changedFile === 'service_worker' ||
-    request.changedFile === '_locales'
-  ) {
-    browser.runtime.reload()
-  }
+    // Reload the extension runtime if the manifest or
+    // service worker changes.
+    if (
+      request.changedFile === 'declarative_net_request' ||
+      request.changedFile === 'manifest.json' ||
+      request.changedFile === 'service_worker' ||
+      request.changedFile === '_locales'
+    ) {
+      browser.runtime.reload()
+    }
 
-  return {reloaded: true}
-})
+    return {reloaded: true}
+  }
+)
