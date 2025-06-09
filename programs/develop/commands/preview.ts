154aed907e069da7a952f3123c7645bf7cf5000b
Cezar Augusto <boss@cezaraugusto.net>
2025-06-09 19:45:34 -0300

    Fix clean function

--- programs/develop/commands/preview.ts
+++ programs/develop/commands/preview.ts
@@ -10,7 +10,9 @@ import * as path from 'path'
 import {rspack, type Configuration} from '@rspack/core'
 import {merge} from 'webpack-merge'
 import webpackConfig from '../webpack/webpack-config'
-import {getProjectPath} from './commands-lib/get-project-path'
+import {
+  getProjectPath,
+} from './commands-lib/get-project-path'
 import * as messages from './commands-lib/messages'
 import {loadCustomWebpackConfig} from './commands-lib/get-extension-config'
 import {PreviewOptions} from './commands-lib/config-types'
@@ -20,6 +22,19 @@ export async function extensionPreview(
   previewOptions: PreviewOptions
 ) {
   const projectPath = await getProjectPath(pathOrRemoteUrl)
+  const distPath = path.join(projectPath, 'dist', previewOptions.browser)
+
+  // Output path defaults to extensionPreview config.
+  // The start command will use the build directory.
+  // The preview command will use the build directory if it exists,
+  // otherwise it will use the project path.
+  // This is useful for remote packages that don't have a build directory.
+  // but are ready for manual browser testing.
+  const outputPath = previewOptions.outputPath
+    ? previewOptions.outputPath
+    : fs.existsSync(distPath)
+      ? distPath
+      : projectPath
 
   if (
     !pathOrRemoteUrl?.startsWith('http') &&
@@ -30,7 +45,7 @@ export async function extensionPreview(
     )
     process.exit(1)
   }
-
+ 
   try {
     const browser = previewOptions.browser || 'chrome'
     const baseConfig: Configuration = webpackConfig(projectPath, {
@@ -43,7 +58,8 @@ export async function extensionPreview(
       // Preview needs a build before running so
       // we don't want to clean the output directory.
       output: {
-        clean: false
+        clean: false,
+        path: outputPath
       }
     })
 
@@ -66,9 +82,7 @@ export async function extensionPreview(
       }
 
       if (!stats?.hasErrors()) {
-        console.log(
-          messages.runningInProduction(path.join(projectPath, 'dist', browser))
-        )
+        console.log(messages.runningInProduction(projectPath))
       } else {
         console.log(stats.toString({colors: true}))
         process.exit(1)
--- programs/develop/webpack/plugin-compilation/clean-dist.ts
+++ programs/develop/webpack/plugin-compilation/clean-dist.ts
@@ -3,12 +3,13 @@ import * as path from 'path'
 import {type Compiler} from '@rspack/core'
 
 export class CleanDistFolderPlugin {
+  constructor(private options: {browser: string}) {}
   apply(compiler: Compiler): void {
-    const outputPath = path.join(compiler.options.output.path || '')
+    const distPath = path.join(compiler.options.context!, 'dist', this.options.browser)
 
-    if (fs.existsSync(outputPath)) {
+    if (fs.existsSync(distPath)) {
       try {
-        fs.rmSync(outputPath, {recursive: true, force: true})
+        fs.rmSync(distPath, {recursive: true, force: true})
         if (process.env.EXTENSION_ENV === 'development') {
           console.log(
             '[CleanDistFolderPlugin] Removed old hot-update files before compilation.'
