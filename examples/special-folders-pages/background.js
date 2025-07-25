d217237f3c7e65188c3e59a18b95656443d3f007
Cezar Augusto <boss@cezaraugusto.net>
2025-07-25 15:41:41 -0300

    Prepare examples for ejection

--- examples/special-folders-pages/background.js
+++ examples/special-folders-pages/background.js
@@ -1,4 +1,5 @@
 chrome.runtime.onInstalled.addListener(() => {
+  console.log('🚀 Special Folders - Pages: Opening pages/main.html on startup')
   chrome.tabs.create({
     url: './pages/main.html'
   })
