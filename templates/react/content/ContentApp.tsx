8dbcf974e04deae8072091862474eef66028fc0a
Cezar Augusto <boss@cezaraugusto.net>
2025-08-08 19:34:57 -0300

    Support 'use shadow-dom' directive for content_scripts

--- templates/react/content/ContentApp.tsx
+++ templates/react/content/ContentApp.tsx
@@ -3,12 +3,8 @@ import logo from '../images/react.png'
 export default function ContentApp() {
   return (
     <div className="content_script">
-      <img className="content_logo" src={logo} alt="Extension Logo" />
-      <h1 className="content_title">
-        Content Script
-        <br />
-        React Extension
-      </h1>
+      <img className="content_logo" src={logo} alt="React Logo" />
+      <h1 className="content_title">React Extension</h1>
       <p className="content_description">
         This content script runs in the context of web pages.
         <br />
