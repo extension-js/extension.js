0e0b6d2ea686fe41a3585714bbe609ff57bae7be
Cezar Augusto <boss@cezaraugusto.net>
2025-10-20 12:28:03 -0300

    Migrate all examples

--- examples/monorepo-turbopack/clients/browser/background.js
+++
@@ -1,6 +0,0 @@
-console.log('Extension.js Monorepo Browser Example background script loaded!')
-
-// Example background script functionality
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('Extension.js Monorepo Browser Example installed')
-})
--- examples/transformers-js/constants.js
+++
@@ -1,2 +0,0 @@
-export const CONTEXT_MENU_ITEM_ID = 'classify-selection'
-export const ACTION_NAME = 'classify'
