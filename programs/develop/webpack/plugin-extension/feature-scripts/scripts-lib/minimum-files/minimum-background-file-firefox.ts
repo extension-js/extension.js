574a42ca77a7b476912c687cee200068ac6d4118
Cezar Augusto <boss@cezaraugusto.net>
2025-11-14 00:04:34 -0300

    Do not print dev-server reload error for service workers

--- programs/develop/webpack/plugin-extension/feature-scripts/scripts-lib/minimum-files/minimum-background-file-firefox.ts
+++ programs/develop/webpack/plugin-extension/feature-scripts/scripts-lib/minimum-files/minimum-background-file-firefox.ts
@@ -1,1 +1,11 @@
+// Guard dev-server client reload in Worker context
+try {
+  const loc: any = (globalThis as any)?.location
+
+  if (loc && typeof loc.reload !== 'function') {
+    loc.reload = () => {}
+  }
+} catch {
+  // ignore
+}
 console.log('Minimum Firefox file loaded for reload purposes')
