e9e271310c25d82db0ae189ae61652e42f9c366b
Cezar Augusto <boss@cezaraugusto.net>
2026-01-16 02:33:59 -0300

    Bring back create/

---
+++ programs/create/steps/initialize-git-repository.ts
@@ -0,0 +1,59 @@
+//  ██████╗██████╗ ███████╗ █████╗ ████████╗███████╗
+// ██╔════╝██╔══██╗██╔════╝██╔══██╗╚══██╔══╝██╔════╝
+// ██║     ██████╔╝█████╗  ███████║   ██║   █████╗
+// ██║     ██╔══██╗██╔══╝  ██╔══██║   ██║   ██╔══╝
+// ╚██████╗██║  ██║███████╗██║  ██║   ██║   ███████╗
+//  ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝
+// MIT License (c) 2020–present Cezar Augusto & the Extension.js authors — presence implies inheritance
+
+import {spawn} from 'cross-spawn'
+import * as messages from '../lib/messages'
+
+export async function initializeGitRepository(
+  projectPath: string,
+  projectName: string
+) {
+  const gitCommand = 'git'
+  const gitArgs = ['init', '--quiet']
+
+  console.log(messages.initializingGitForRepository(projectName))
+
+  try {
+    const stdio =
+      process.env.EXTENSION_ENV === 'development' ? 'inherit' : 'ignore'
+    const child = spawn(gitCommand, gitArgs, {
+      stdio,
+      cwd: projectPath
+    })
+
+    await new Promise<void>((resolve, reject) => {
+      child.on('close', (code) => {
+        if (code !== 0) {
+          reject(
+            new Error(
+              messages.initializingGitForRepositoryFailed(
+                gitCommand,
+                gitArgs,
+                code
+              )
+            )
+          )
+        } else {
+          resolve()
+        }
+      })
+
+      child.on('error', (error) => {
+        console.error(
+          messages.initializingGitForRepositoryProcessError(projectName, error)
+        )
+        reject(error)
+      })
+    })
+  } catch (error: any) {
+    console.error(
+      messages.initializingGitForRepositoryError(projectName, error)
+    )
+    throw error
+  }
+}
