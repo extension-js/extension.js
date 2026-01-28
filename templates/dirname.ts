ee22ebf4f74e193029ae1826f50a9b8a204652f2
Cezar Augusto <boss@cezaraugusto.net>
2026-01-28 09:44:20 -0300

    Update internal templates

--- templates/dirname.ts
+++
@@ -1,7 +0,0 @@
-import {fileURLToPath} from 'url'
-import * as path from 'path'
-
-export function getDirname(importMetaUrl: string) {
-  const __filename = fileURLToPath(importMetaUrl)
-  return path.dirname(__filename)
-}
