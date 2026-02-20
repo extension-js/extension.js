0418152d87aea58eccfd9c2ed3f22a00ebc392ae
Cezar Augusto <boss@cezaraugusto.net>
2026-02-20 13:42:54 -0300

    Reduce dependency complexity

--- templates/dirname.js
+++
@@ -1,7 +0,0 @@
-import {fileURLToPath} from 'url'
-import path from 'path'
-
-export function getDirname(importMetaUrl) {
-  const filename = fileURLToPath(importMetaUrl)
-  return path.dirname(filename)
-}
