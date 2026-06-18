dfc2be116232a5c0946958d5f2a9095c97eced66
Cezar Augusto <boss@cezaraugusto.net>
2026-06-18 18:15:47 -0300

    Type 41 manifest-shape casts to drop any so wrong-shaped manifests fail the compiler

--- programs/develop/plugin-web-extension/feature-manifest/manifest-overrides/mv3/host_permissions.ts
+++ programs/develop/plugin-web-extension/feature-manifest/manifest-overrides/mv3/host_permissions.ts
@@ -10,8 +10,8 @@ import {type Manifest} from '../../../../types'
 
 export function hostPermissions(manifest: Manifest) {
   return (
-    (manifest as any).host_permissions && {
-      host_permissions: (manifest as any).host_permissions
+    manifest.host_permissions && {
+      host_permissions: manifest.host_permissions
     }
   )
 }
